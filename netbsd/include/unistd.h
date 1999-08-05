e37b3fb69be72839a4a81bfff93a698b0c5df847
itojun <itojun>
1999-08-05 16:09:12 +0000

    sync with KAME/NetBSD.

--- netbsd/include/unistd.h
+++ netbsd/include/unistd.h
@@ -326,6 +326,12 @@ int	 syscall __P((int, ...));
 quad_t	 __syscall __P((quad_t, ...));
 int	 undelete __P((const char *));
 
+#if 1 /*INET6*/
+int	 rresvport_af __P((int *, int));
+int	 ruserok_af __P((const char *, int, const char *, const char *, int));
+int	 iruserok_af __P((const void *, int, const char *, const char *, int));
+#endif
+
 extern __const char *__const *sys_siglist __RENAME(__sys_siglist14);
 extern	 int optreset;		/* getopt(3) external variable */
 extern	 char *suboptarg;	/* getsubopt(3) external variable */
