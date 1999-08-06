b1108c2b967a68d2a7281d1388d80050c996ab16
itojun <itojun>
1999-08-06 08:01:18 +0000

    FreeBSD 2.2.8-RELEASE

---
+++ freebsd2/lib/libskey/pathnames.h
@@ -0,0 +1,6 @@
+/* $Id: pathnames.h,v 1.2 1994/10/19 00:03:14 pst Exp $ (FreeBSD) */
+
+#include <paths.h>
+
+#define _PATH_SKEYACCESS        "/etc/skey.access"
+#define	_PATH_SKEYFILE		"/etc/skeykeys"
