256b66f6e6c3f9320c9c4424341fd1483e53ec2b
itojun <itojun>
1999-08-06 04:06:00 +0000

    warn if non-IPv6 sockaddr is passed to udp6_notify.
    XXX is it meaningful to log and proceed?  should we panic()?

--- kame/sys/netinet6/udp6_usrreq.c
+++ kame/sys/netinet6/udp6_usrreq.c
@@ -78,9 +78,7 @@
 #ifdef __NetBSD__
 #include <sys/proc.h>
 #endif
-#ifdef __FreeBSD__
 #include <sys/syslog.h>
-#endif
 
 #include <net/if.h>
 #include <net/route.h>
@@ -478,6 +476,10 @@ udp6_ctlinput(cmd, sa, ip6, m, off)
 		return;
 
 	/* translate addresses into internal form */
+	if (sa->sa_family != AF_INET6 ||
+	    sa->sa_len != sizeof(struct sockaddr_in6))
+		log(LOG_ERR, "udp6_ctlinput: arg sa is not for IPv6,"
+		    "sa->sa_family = %d, len = %d", sa->sa_family, sa->sa_len);
 	sa6 = *(struct sockaddr_in6 *)sa;
 	if (IN6_IS_ADDR_LINKLOCAL(&sa6.sin6_addr))
 		sa6.sin6_addr.s6_addr16[1] = htons(m->m_pkthdr.rcvif->if_index);
