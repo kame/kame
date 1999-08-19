48ab05df062a9154d94157cf64a2149d12f5bfda
itojun <itojun>
1999-08-19 09:01:41 +0000

    make it spec conformant when updating per-interface stat for inbound fragmented packet.

--- kame/sys/netinet6/frag6.c
+++ kame/sys/netinet6/frag6.c
@@ -53,6 +53,13 @@
 
 #include <net/net_osdep.h>
 
+/*
+ * Define it to get a correct behavior on per-interface statistics.
+ * You will need to perform an extra routing table lookup, per fragment,
+ * to do it.  This may, or may not be, a performance hit.
+ */
+#define IN6_IFSTAT_STRICT
+
 static void frag6_enq __P((struct ip6asfrag *, struct ip6asfrag *));
 static void frag6_deq __P((struct ip6asfrag *));
 static void frag6_insque __P((struct ip6q *, struct ip6q *));
@@ -98,15 +105,50 @@ frag6_input(mp, offp, proto)
 	int offset = *offp, nxt, i, next;
 	int first_frag = 0;
 	u_short fragoff, frgpartlen;
+	struct ifnet *dstifp;
+#ifdef IN6_IFSTAT_STRICT
+	static struct route_in6 ro;
+	struct sockaddr_in6 *dst;
+#endif
 
 	IP6_EXTHDR_CHECK(m, offset, sizeof(struct ip6_frag), IPPROTO_DONE);
 
 	ip6 = mtod(m, struct ip6_hdr *);
 	ip6f = (struct ip6_frag *)((caddr_t)ip6 + offset);
 
+	dstifp = NULL;
+#ifdef IN6_IFSTAT_STRICT
+	/* find the destination interface of the packet. */
+	dst = (struct sockaddr_in6 *)&ro.ro_dst;
+	if (ro.ro_rt
+	 && ((ro.ro_rt->rt_flags & RTF_UP) == 0
+	  || !IN6_ARE_ADDR_EQUAL(&dst->sin6_addr, &ip6->ip6_dst))) {
+		RTFREE(ro.ro_rt);
+		ro.ro_rt = (struct rtentry *)0;
+	}
+	if (ro.ro_rt == NULL) {
+		bzero(dst, sizeof(*dst));
+		dst->sin6_family = AF_INET6;
+		dst->sin6_len = sizeof(struct sockaddr_in6);
+		dst->sin6_addr = ip6->ip6_dst;
+	}
+#ifdef __NetBSD__
+	rtalloc((struct route *)&ro);
+#else
+	rtcalloc((struct route *)&ro);
+#endif
+	if (ro.ro_rt != NULL)
+		dstifp = ro.ro_rt->rt_ifp;
+#else
+	/* we are violating the spec, this is not the destination interface */
+	if ((m->m_flags & M_PKTHDR) != 0)
+		dstifp = m->m_pkthdr.rcvif;
+#endif
+
 	/* jumbo payload can't contain a fragment header */
 	if (ip6->ip6_plen == 0) {
 		icmp6_error(m, ICMP6_PARAM_PROB, ICMP6_PARAMPROB_HEADER, offset);
+		in6_ifstat_inc(dstifp, ifs6_reass_fail);
 		return IPPROTO_DONE;
 	}
 
@@ -121,13 +163,12 @@ frag6_input(mp, offp, proto)
 		icmp6_error(m, ICMP6_PARAM_PROB,
 			    ICMP6_PARAMPROB_HEADER,
 			    (caddr_t)&ip6->ip6_plen - (caddr_t)ip6);
+		in6_ifstat_inc(dstifp, ifs6_reass_fail);
 		return IPPROTO_DONE;
 	}
 
 	ip6stat.ip6s_fragments++;
-	/* XXX wrong ifp */
-	if (m->m_flags & M_PKTHDR)
-		in6_ifstat_inc(m->m_pkthdr.rcvif, ifs6_reass_reqd);
+	in6_ifstat_inc(dstifp, ifs6_reass_reqd);
 	
 	/*
 	 * Presence of header sizes in mbufs
@@ -159,9 +200,7 @@ frag6_input(mp, offp, proto)
 		 */
 		if (frag6_nfragpackets >= (u_int)ip6_maxfragpackets) {
 			ip6stat.ip6s_fragoverflow++;
-			/* XXX wrong ifp */
-			if (m->m_flags & M_PKTHDR)
-				in6_ifstat_inc(m->m_pkthdr.rcvif, ifs6_reass_fail);
+			in6_ifstat_inc(dstifp, ifs6_reass_fail);
 			frag6_freef(ip6q.ip6q_prev);
 		}
 		q6 = (struct ip6q *)malloc(sizeof(struct ip6q), M_FTABLE,
@@ -434,9 +473,7 @@ insert:
 	}
 	
 	ip6stat.ip6s_reassembled++;
-	/*XXX wrong ifp */
-	if (m->m_flags & M_PKTHDR)
-		in6_ifstat_inc(m->m_pkthdr.rcvif, ifs6_reass_ok);
+	in6_ifstat_inc(dstifp, ifs6_reass_ok);
 
 	/*
 	 * Tell launch routine the next header
@@ -449,6 +486,7 @@ insert:
 	return nxt;
 
  dropfrag:
+	in6_ifstat_inc(dstifp, ifs6_reass_fail);
 	ip6stat.ip6s_fragdropped++;
 	m_freem(m);
 	return IPPROTO_DONE;
