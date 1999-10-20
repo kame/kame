3b6669099d5d71f9b37f6fc5cc6b41171c5fa09b
sakane <sakane>
1999-10-20 13:57:07 +0000

    improved to set policy.

--- freebsd2/usr.bin/telnet/commands.c
+++ freebsd2/usr.bin/telnet/commands.c
@@ -2144,6 +2144,36 @@ sockaddr_ntop(sa)
     return inet_ntop(sa->sa_family, addr, addrbuf, sizeof(addrbuf));
 }
 
+#if defined(IPSEC) && defined(IPSEC_POLICY_IPSEC)
+static int
+setpolicy(net, res, policy)
+	int net;
+	struct addrinfo *res;
+	char *policy;
+{
+	char *buf;
+	int level;
+	int optname;
+
+    	if (policy == NULL)
+		return 0;
+
+	buf = ipsec_set_policy(policy, strlen(policy));
+	if (buf == NULL) {
+		printf("%s\n", ipsec_strerror());
+		return -1;
+	}
+	level = res->ai_family == AF_INET ? IPPROTO_IP : IPPROTO_IPV6;
+	optname = res->ai_family == AF_INET ? IP_IPSEC_POLICY : IPV6_IPSEC_POLICY;
+	if (setsockopt(net, level, optname, buf, ipsec_get_policylen(buf)) < 0){
+		perror("setsockopt");
+		return -1;
+	}
+
+	free(buf);
+}
+#endif
+
 int
 tn(argc, argv)
     int argc;
@@ -2286,43 +2316,10 @@ tn(argc, argv)
 		perror("setsockopt (source route)");
 	}
 #if defined(IPSEC) && defined(IPSEC_POLICY_IPSEC)
-    {
-	int len;
-	char *buf;
-	int level;
-	int optname;
-
-    	if (ipsec_policy_in != NULL) {
-		buf = ipsec_set_policy(ipsec_policy_in, strlen(ipsec_policy_in));
-		if (buf == NULL) {
-			printf("%s\n", ipsec_strerror());
-			return 0;
-		}
-		level = res->ai_family == AF_INET ? IPPROTO_IP : IPPROTO_IPV6;
-		optname = res->ai_family == AF_INET ? IP_IPSEC_POLICY : IPV6_IPSEC_POLICY;
-		if (setsockopt(net, level, optname,
-				buf, ipsec_get_policylen(buf)) < 0){
-			perror("setsockopt");
-			return 0;
-		}
-		free(buf);
-	}
-    	if (ipsec_policy_out != NULL) {
-		buf = ipsec_set_policy(ipsec_policy_out, strlen(ipsec_policy_out));
-		if (buf == NULL) {
-			printf("%s\n", ipsec_strerror());
-			return 0;
-		}
-		level = res->ai_family == AF_INET ? IPPROTO_IP : IPPROTO_IPV6;
-		optname = res->ai_family == AF_INET ? IP_IPSEC_POLICY : IPV6_IPSEC_POLICY;
-		if (setsockopt(net, level, optname,
-				buf, ipsec_get_policylen(buf)) < 0){
-			perror("setsockopt");
-			return 0;
-		}
-		free(buf);
-	}
-    }
+	if (setpolicy(net, res, ipsec_policy_in) < 0)
+		return 0;
+	if (setpolicy(net, res, ipsec_policy_out) < 0)
+		return 0;
 #endif
 
 	if (connect(net, res->ai_addr, res->ai_addrlen) < 0) {
