23d21059c4cf8c1755e0ddf5d16ab01f7e07d03f
itojun <itojun>
1999-10-23 01:44:38 +0000

    do not bark if ipsec policy is not explicitly specified.

--- freebsd2/usr.sbin/inetd/inetd.c
+++ freebsd2/usr.sbin/inetd/inetd.c
@@ -1098,7 +1098,8 @@ ipsecsetup(sep)
 		buf = ipsec_set_policy(policy_in, strlen(policy_in));
 		if (buf != NULL) {
 			if (setsockopt(sep->se_fd, level, opt,
-					buf, ipsec_get_policylen(buf)) < 0) {
+					buf, ipsec_get_policylen(buf)) < 0
+			 && sep->se_policy) {
 				syslog(LOG_ERR,
 					"%s/%s: ipsec initialization failed; %s",
 					sep->se_service, sep->se_proto,
@@ -1115,7 +1116,8 @@ ipsecsetup(sep)
 		buf = ipsec_set_policy(policy_out, strlen(policy_out));
 		if (buf != NULL) {
 			if (setsockopt(sep->se_fd, level, opt,
-					buf, ipsec_get_policylen(buf)) < 0) {
+					buf, ipsec_get_policylen(buf)) < 0
+			 && sep->se_policy) {
 				syslog(LOG_ERR,
 					"%s/%s: ipsec initialization failed; %s",
 					sep->se_service, sep->se_proto,
