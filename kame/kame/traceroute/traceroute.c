e6d72541ef99f78c74b82c7c383916bca0f66ff3
itojun <itojun>
1999-11-02 17:13:03 +0000

    obey configure's #define for netbsd.

--- kame/kame/traceroute/traceroute.c
+++ kame/kame/traceroute/traceroute.c
@@ -24,7 +24,7 @@ static const char copyright[] =
     "@(#) Copyright (c) 1988, 1989, 1991, 1994, 1995, 1996, 1997\n\
 The Regents of the University of California.  All rights reserved.\n";
 static const char rcsid[] =
-    "@(#)$Header: /usr/home/sumikawa/kame/kame/kame/kame/traceroute/traceroute.c,v 1.9 1999/10/21 04:08:12 sakane Exp $ (LBL)";
+    "@(#)$Header: /usr/home/sumikawa/kame/kame/kame/kame/traceroute/traceroute.c,v 1.10 1999/11/02 17:13:03 itojun Exp $ (LBL)";
 #endif
 
 /*
@@ -1001,7 +1001,7 @@ send_probe(register int seq, int ttl, register struct timeval *tp)
 			tip = *outip;
 			ui = (struct udpiphdr *)outip;
 #ifdef HAVE_UDPIPHDR_NEXT
-#if !((defined(__FreeBSD__) && __FreeBSD__ >= 3) || defined(__NetBSD__))
+#if !(defined(__FreeBSD__) && __FreeBSD__ >= 3)
 			ui->ui_next = 0;
 			ui->ui_prev = 0;
 #endif
