c7bfeadd49b4b71f559403546c2c5e267c6bf887
jinmei <jinmei>
1999-11-24 11:27:52 +0000

    format fix

--- kame/kame/pim6sd/debug.c
+++ kame/kame/pim6sd/debug.c
@@ -448,11 +448,16 @@ dump_nbrs(fp)
 
 	for (vifi = 0, v = uvifs; vifi < numvifs; ++vifi, ++v) {
 		if ((n = v->uv_pim_neighbors) != NULL) {
+			int first = 1;
+
 			fprintf(fp, " %-3u %6s", vifi,
 				(v->uv_flags & MIFF_REGISTER)?"regist":
 				v->uv_name);
 			for (; n != NULL; n = n->next) {
-				fprintf(fp, " %3s %6s", "", "");
+				if (first)
+					first == 0;
+				else
+					fprintf(fp, " %3s %6s", "", "");
 				fprintf(fp, " %-40s %-5u\n",
 					inet6_fmt(&n->address.sin6_addr),
 					n->timer);
