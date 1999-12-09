a7f781b4cfa815b130b0672a74d7d658ed684410
itojun <itojun>
1999-12-09 01:00:30 +0000

    add ETHERTYPE_IPV6 for compilation on linux.

--- kame/kame/libpcap/linux-include/netinet/if_ether.h
+++ kame/kame/libpcap/linux-include/netinet/if_ether.h
@@ -53,6 +53,7 @@ struct	ether_header {
 
 #define	ETHERTYPE_PUP		0x0200	/* PUP protocol */
 #define	ETHERTYPE_IP		0x0800	/* IP protocol */
+#define	ETHERTYPE_IPV6		0x86dd	/* IPv6 protocol */
 #define ETHERTYPE_ARP		0x0806	/* Addr. resolution protocol */
 #define ETHERTYPE_REVARP	0x8035	/* reverse Addr. resolution protocol */
 
