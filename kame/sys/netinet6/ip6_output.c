bc9cac8b57a7d68557c8927045ec00b505e6f3ba
jinmei <jinmei>
1999-12-24 01:10:07 +0000

    enabled IPV6_REACHCONF cases in ip6_ctloutput().

--- kame/sys/netinet6/ip6_output.c
+++ kame/sys/netinet6/ip6_output.c
@@ -1359,11 +1359,11 @@ ip6_ctloutput(op, so, level, optname, mp)
 			case IPV6_RECVPKTINFO:
 			case IPV6_RECVHOPLIMIT:
 			case IPV6_RECVRTHDR:
+			case IPV6_REACHCONF:
 #ifdef notyet			/* To be implemented */
 			case IPV6_RTHDRDSTOPTS:
 			case IPV6_USE_MIN_MTU:
 			case IPV6_RECVPATHMTU:
-			case IPV6_REACHCONF:
 #endif 
 #ifdef MAPPED_ADDR_ENABLED
 			case IPV6_BINDV6ONLY:
@@ -1698,11 +1698,11 @@ ip6_ctloutput(op, so, level, optname, mp)
 			case IPV6_RECVPKTINFO:
 			case IPV6_RECVHOPLIMIT:
 			case IPV6_RECVRTHDR:
+			case IPV6_REACHCONF:
 #ifdef notyet			/* To be implemented */
 			case IPV6_RTHDRDSTOPTS:
 			case IPV6_USE_MIN_MTU:
 			case IPV6_RECVPATHMTU:
-			case IPV6_REACHCONF:
 #endif 
 
 			case IPV6_FAITH:
