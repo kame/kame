89899f517d8c9619be0ad72737191a2a09f0c48c
itojun <itojun>
2000-02-04 04:47:20 +0000

    v6regex: to show how hard it is to perform v6 numeric address regex match.

---
+++ kame/kame/v6regex/testsuite.sh
@@ -0,0 +1,51 @@
+interface=ne2
+
+echo '==> valid IPv4 address'
+./v6regex 127.0.0.1
+./v6regex 0.0.0.0
+./v6regex 255.255.255.255
+
+echo '==> still valid'
+./v6regex
+
+echo '==> invalid IPv4 address'
+./v6regex 999.999.999.999
+./v6regex 255.255.255.256
+
+echo '==> valid IPv6 address'
+./v6regex ::
+./v6regex ::1
+./v6regex 0::
+./v6regex 0::0
+./v6regex 0:1:2:3:4:5:6:7
+./v6regex ::10.1.1.1
+./v6regex ::ffff:10.1.1.1
+./v6regex ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff
+./v6regex 0000:0000:0000:0000:0000:0000:0000:0000
+
+echo '==> invalid IPv6 address'
+./v6regex ::10.1.1
+./v6regex 0::0:0:0::0
+./v6regex ffff:ffff:ffff:ffff:ffff:ffff:ffff:fffff
+./v6regex gggg:gggg:gggg:gggg:gggg:gggg:gggg:gggg
+
+echo '==> with KAME scoped'
+./v6regex fe80::1@$interface
+
+echo '==> with KAME scoped, should be considered invalid.'
+# site-local is a bit controversial but the code does not support it yet.
+./v6regex fe80::1@10000
+./v6regex fe80::1@mumbojumbo
+./v6regex fec0::1@$interface
+./v6regex 3ffe::1@$interface
+
+echo '==> with new scoped proposal'
+./v6regex 10%fe80::1
+./v6regex $interface%fe80::1
+
+echo '==> with new scoped proposal, should be considered invalid'
+# again, site-local is a bit controversial.
+./v6regex 10000%fe80::1
+./v6regex mumbojumbo%fe80::1
+./v6regex $interface%fec0::1
+./v6regex $interface%3ffe::1
