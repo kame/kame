1daa4acef100bb0c7409ed4156397c423768b70a
sakane <sakane>
2000-02-07 11:16:18 +0000

    remove comments in signing.h.  comments should be in signing.c.

--- kame/kame/racoon/signing.h
+++ kame/kame/racoon/signing.h
@@ -6,61 +6,8 @@
 #define CERTFILE	"cert.pem"
 #define CERTREQFILE	"cr.pem"
 
+extern int sign __P((void *, int , char *, char **, int *));
+extern int check_signature __P((void *, int , char *, char *, int ));
+extern int get_certificate __P((char *, int *, char **));
+extern int check_certificate __P((char *, char *, int ));
 
-/*
- *	sign is a signature function
- *	Input: 	void *input_data
- *		int size_of_input_data
- *		char *user
- *	Output:	char *signature
- *		int *signature_size
- *
- *	User will be used to fetch the user signing key. If such user
- *	does not exist, or the key is not correctly retrieved, then "sign"
- *	returns -1. If everything goes well, it returns 0.
- */
-int sign(void *, int , char *, char **, int *);
-
-
-/*
- *	check_signature is a signature checking function
- *	Input: 	void *input_data
- *		int   input_data_size
- *		char *user
- *		char *signature
- *		int signature_size
- *
- *	User will be used to fetch the user public key certificate. If
- *	such user does not exist, or the public key is not correctly retrieved
- *	from the certificate, then "sign" returns -1. If everything goes
- *	well, it returns 0.
- */
-int check_signature(void *, int , char *, char *, int );
-
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// 					TO GET A CERTIFICATE
-// Input:
-//	a user name "user" (we check his public key certificate)
-// Output:
-//	return int: 0 if the cert is OK or -1 otherwise.
-//	int *: set certificate_size
-//	char **: points to a certificate pointer
-//////////////////////////////////////////////////////////////////////////////////////////
-int get_certificate(char *, int *, char **);
-
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// 					MAIN CERTIFICATE CHECKING FUNCTION
-// Input:
-//	a char * user name
-// 		precise the location of the to-be-stored certificate
-//		In case there was already a certificate here, it is erased.
-//	a char * certificate to check
-//	its size int
-//
-// Output:
-//	return int: 0 if cert is OK or -1 otherwise.
-//
-//////////////////////////////////////////////////////////////////////////////////////////
-int check_certificate(char *, char *, int );
