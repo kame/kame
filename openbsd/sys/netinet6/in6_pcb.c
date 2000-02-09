fd3c06253ad6dc21ff771616ff934c572b031861
itojun <itojun>
2000-02-09 13:13:58 +0000

    fix include file pat

--- openbsd/sys/netinet6/in6_pcb.c
+++ openbsd/sys/netinet6/in6_pcb.c
@@ -72,7 +72,7 @@ didn't get a copy, you may request one from <license@ipv6.nrl.navy.mil>.
 #include <netinet/in_pcb.h>
 
 #include <netinet6/in6_var.h>
-#include <netinet6/ip6.h>
+#include <netinet/ip6.h>
 #include <netinet6/ip6_var.h>
 
 #if __OpenBSD__
