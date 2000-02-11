12ffe5394faf464d8bdad7de9fb60f5283e7b520
itojun <itojun>
2000-02-11 12:33:25 +0000

    s/__ss_{len,family}/ss_{len,family}/

--- kame/kame/libinet6/missing/sockstorage.h
+++ kame/kame/libinet6/missing/sockstorage.h
@@ -28,8 +28,8 @@
  */
 struct sockaddr_storage {
 #ifdef HAVE_SOCKADDR_SA_LEN
-	u_int8_t __ss_len;
-	u_int8_t __ss_family;
+	u_int8_t ss_len;
+	u_int8_t ss_family;
 	u_int8_t fill[126];
 #else
 	u_int8_t __ss_family;
