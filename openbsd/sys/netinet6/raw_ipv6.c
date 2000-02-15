953942070ee73efa87f20974f660349cdb7d456b
jinmei <jinmei>
2000-02-15 13:25:45 +0000

    check icmp6 filter for icmp6 packets only.

--- openbsd/sys/netinet6/raw_ipv6.c
+++ openbsd/sys/netinet6/raw_ipv6.c
@@ -373,7 +373,8 @@ rip6_input(mp, offp, proto)
     if (!IN6_IS_ADDR_UNSPECIFIED(&inp->inp_faddr6) && 
 	!IN6_ARE_ADDR_EQUAL(&inp->inp_faddr6, &ip6->ip6_src))
       continue;
-    if (inp->inp_icmp6filt && 
+    /* inp_icmp6filt must not be NULL, but we add a check for safety */
+    if ((icmp6type > 0) && inp->inp_icmp6filt && 
 	ICMP6_FILTER_WILLBLOCK(icmp6type, inp->inp_icmp6filt))
       continue;
 
