0da4c1fd53aa45921df7dc2620930b0e15588af3
itojun <itojun>
2000-02-29 03:17:01 +0000

    call encap_init.
    XXX in "options INET" case, we need to do it in in_proto.c.
    we need to call it only once (it if not harmful to call it multiple times,
    though).

--- kame/sys/netinet6/in6_proto.c
+++ kame/sys/netinet6/in6_proto.c
@@ -1,4 +1,4 @@
-/*	$KAME: in6_proto.c,v 1.40 2000/02/24 16:34:49 itojun Exp $	*/
+/*	$KAME: in6_proto.c,v 1.41 2000/02/29 03:17:01 itojun Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -351,7 +351,12 @@ struct ip6protosw inet6sw[] = {
 #else
   rip6_usrreq,
 #endif
-  0,		0,		0,		0,
+#ifndef INET
+  encap_init,
+#else
+  0,
+#endif
+  0,		0,		0,
 #if defined(__FreeBSD__) && __FreeBSD__ >= 3
   &rip6_usrreqs
 #endif
