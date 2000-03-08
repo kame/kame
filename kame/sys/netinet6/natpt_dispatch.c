2f5389a8271801a3bca02708f16a6dc32131bc7d
itojun <itojun>
2000-03-08 04:00:17 +0000

    sanity check on header parsing (check m_len). don't parse through fragment header.  it makes no sense to parse it unless it is the first fragment.

--- kame/sys/netinet6/natpt_dispatch.c
+++ kame/sys/netinet6/natpt_dispatch.c
@@ -1,4 +1,4 @@
-/*	$KAME: natpt_dispatch.c,v 1.6 2000/02/22 14:04:27 itojun Exp $	*/
+/*	$KAME: natpt_dispatch.c,v 1.7 2000/03/08 04:00:17 itojun Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -28,7 +28,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	$Id: natpt_dispatch.c,v 1.6 2000/02/22 14:04:27 itojun Exp $
+ *	$Id: natpt_dispatch.c,v 1.7 2000/03/08 04:00:17 itojun Exp $
  */
 
 #include <sys/param.h>
@@ -465,16 +465,18 @@ foundFinalPayload(struct mbuf *m, int *proto, int *offset)
     nxt = ip6->ip6_nxt;
     off = sizeof(struct ip6_hdr);
     ip6ext = (struct ip6_ext *)((struct ip6_hdr *)(ip6 + 1));
-    while (nxt != IPPROTO_NONE)
+    while (nxt != IPPROTO_NONE && off + sizeof(*ip6ext) < m->m_len)
     {
 	switch (nxt)
 	{
 	  case IPPROTO_HOPOPTS:
 	  case IPPROTO_ROUTING:
+#if 0
 	  case IPPROTO_FRAGMENT:
+#endif
 	  case IPPROTO_DSTOPTS:
 	    nxt = ip6ext->ip6e_nxt;
-	    off = ip6ext->ip6e_len;
+	    off += ip6ext->ip6e_len;
 	    ip6ext = (struct ip6_ext *)(((caddr_t)ip6ext) + ip6ext->ip6e_len);
 	    break;
 
