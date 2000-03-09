1ab49fa506a99a5f2f39e8ecfe8bbb68b7d48f98
sakane <sakane>
2000-03-09 07:52:55 +0000

    improved test program for ipsec_{set|get}_policy.

--- kame/kame/libipsec/test-policy.c
+++ kame/kame/libipsec/test-policy.c
@@ -40,90 +40,108 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
 #include <err.h>
 
-char *requests[] = {
-"must_error",				/* error */
-"in ipsec must_error",			/* error */
-"out ipsec esp/must_error",		/* error */
-"out discard",
-"out none",
-"in entrust",
-"out entrust",
-"in bypass",				/* may be error */
-"out ipsec esp",			/* error */
-"in ipsec ah/transport",
-"in ipsec ah/tunnel",			/* error */
-"out ipsec ah/transport/",
-"out ipsec ah/tunnel/",			/* error */
-"in ipsec esp / transport / 10.0.0.1-10.0.0.2",
-"in ipsec esp/tunnel/::1-::2",
-"in ipsec esp/tunnel/10.0.0.1-::2",	/* error */
-"in ipsec esp/tunnel/::1-::2/require",
-"out ipsec ah/transport//use",
-"out ipsec ah/transport esp/use",
-"in ipsec ah/transport esp/tunnel",	/* error */
-"in ipsec
+struct req_t {
+	int result;	/* expected result; 0:ok 1:ng */
+	char *str;
+} reqs[] = {
+#if 0
+{ 1, "must_error" },
+{ 1, "in ipsec must_error" },
+{ 1, "out ipsec esp/must_error" },
+{ 1, "out discard" },
+{ 1, "out none" },
+{ 0, "in entrust" },
+{ 0, "out entrust" },
+{ 1, "out ipsec esp" },
+{ 0, "in ipsec ah/transport" },
+{ 1, "in ipsec ah/tunnel" },
+{ 0, "out ipsec ah/transport/" },
+{ 1, "out ipsec ah/tunnel/" },
+{ 0, "in ipsec esp / transport / 10.0.0.1-10.0.0.2" },
+{ 0, "in ipsec esp/tunnel/::1-::2" },
+{ 1, "in ipsec esp/tunnel/10.0.0.1-::2" },
+#endif
+{ 0, "in ipsec esp/tunnel/::1-::2/require" },
+#if 0
+{ 0, "out ipsec ah/transport//use" },
+{ 0, "out ipsec ah/transport esp/use" },
+{ 1, "in ipsec ah/transport esp/tunnel" },
+{ 0, "in ipsec
 	ah / transport
-	esp / tunnel / ::1-::2",
-"
-out ipsec
-ah/transport/::1-::2 esp/tunnel/::3-::4/use ah/transport/::5-::6/require
-ah/transport/::1-::2 esp/tunnel/::3-::4/use ah/transport/::5-::6/require
-ah/transport/::1-::2 esp/tunnel/::3-::4/use ah/transport/::5-::6/require
-",
-"out ipsec esp/transport/fec0::10-fec0::11/use",
+	esp / tunnel / ::1-::2" },
+{ 0, "out ipsec
+	ah/transport/::1-::2 esp/tunnel/::3-::4/use ah/transport/::5-::6/require
+	ah/transport/::1-::2 esp/tunnel/::3-::4/use ah/transport/::5-::6/require
+	ah/transport/::1-::2 esp/tunnel/::3-::4/use ah/transport/::5-::6/require
+	" },
+{ 0, "out ipsec esp/transport/fec0::10-fec0::11/use" },
+#endif
 };
 
-int test(char *buf, int family);
+int test1 __P((struct req_t *req));
+int test1sub __P((char *buf, int family));
 
 int
 main(ac, av)
 	int ac;
 	char **av;
 {
-	int do_setsockopt;
-	char *buf;
 	int i;
+	int result;
 
-	if (ac != 1)
-		do_setsockopt = 1;
-	else
-		do_setsockopt = 0;
+	for (i = 0; i < sizeof(reqs)/sizeof(reqs[0]); i++) {
+		printf("#%d [%s]\n", i + 1, reqs[i].str);
 
-	for (i = 0; i < sizeof(requests)/sizeof(requests[0]); i++) {
-		printf("*** requests ***\n");
-		printf("\t[%s]\n", requests[i]);
-
-		buf = ipsec_set_policy(requests[i], strlen(requests[i]));
-		if (buf == NULL) {
-			printf("ipsec_set_policy: %s\n", ipsec_strerror());
-			continue;
+		result = test1(&reqs[i]);
+		if (result == 0 && reqs[i].result == 1) {
+			printf("ERROR: expecting failure.\n");
+		} else if (result == 1 && reqs[i].result == 0) {
+			printf("ERROR: expecting success.\n");
 		}
-
-		printf("\tsetlen:%d\n", ipsec_get_policylen(buf));
-
-		if (do_setsockopt) {
-			printf("\tPF_INET:\n");
-			test(buf, PF_INET);
-
-			printf("\tPF_INET6:\n");
-			test(buf, PF_INET6);
-		} else {
-			kdebug_sadb_x_policy((struct sadb_ext *)buf);
-		}
-		free(buf);
 	}
 
+	exit(0);
+}
+
+int
+test1(req)
+	struct req_t *req;
+{
+	char *buf;
+
+	buf = ipsec_set_policy(req->str, strlen(req->str));
+	if (buf == NULL) {
+		printf("ipsec_set_policy: %s\n", ipsec_strerror());
+		return 1;
+	}
+
+#if 0
+	printf("\tsetlen:%d\n", ipsec_get_policylen(buf));
+#endif
+
+	if (test1sub(buf, PF_INET) != 0
+	 || test1sub(buf, PF_INET6) != 0) {
+		free(buf);
+		return 1;
+	}
+#if 0
+	kdebug_sadb_x_policy((struct sadb_ext *)buf);
+#endif
+
+	free(buf);
 	return 0;
 }
 
 int
-test(policy, family)
+test1sub(policy, family)
 	char *policy;
 	int family;
 {
-	int so, proto, optname;
+	int so;
+	int proto = 0, optname = 0;
 	int len;
 	char getbuf[1024];
 
@@ -143,34 +161,37 @@ test(policy, family)
 
 	len = ipsec_get_policylen(policy);
 	if (setsockopt(so, proto, optname, policy, len) < 0) {
-		printf("error on setsockopt");
-		goto end;
+		printf("fail to set sockopt; %s\n", strerror(errno));
+		close(so);
+		return 1;
 	}
 
-	len = sizeof(getbuf);
 	memset(getbuf, 0, sizeof(getbuf));
+	memcpy(getbuf, policy, sizeof(struct sadb_x_policy));
 	if (getsockopt(so, proto, optname, getbuf, &len) < 0) {
-		printf("error on getsockopt");
-		goto end;
+		printf("fail to get sockopt; %s\n", strerror(errno));
+		close(so);
+		return 1;
 	}
 
     {
 	char *buf = NULL;
 
+#if 0
 	printf("\tgetlen:%d\n", len);
+#endif
 
 	if ((buf = ipsec_dump_policy(getbuf, NULL)) == NULL) {
 		printf("%s\n", ipsec_strerror());
-		goto end;
-	} else {
-		printf("\t[%s]\n", buf);
-		free(buf);
+		close(so);
+		return 1;
 	}
+
+	printf("\t[%s]\n", buf);
+	free(buf);
     }
 
-    end:
 	close (so);
-
 	return 0;
 }
 
