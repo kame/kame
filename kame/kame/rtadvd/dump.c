415256ded040365a03bb3748a7709b5e09d633d8
k-sugyou <k-sugyou>
2000-03-28 08:05:50 +0000

    add net/if_var.h for fbsd3.4

--- kame/kame/rtadvd/dump.c
+++ kame/kame/rtadvd/dump.c
@@ -30,6 +30,9 @@
 #include <sys/socket.h>
 
 #include <net/if.h>
+#if defined(__FreeBSD__) && __FreeBSD__ >= 3
+#include <net/if_var.h>
+#endif /* __FreeBSD__ >= 3 */
 #include <net/if_dl.h>
 
 #include <netinet/in.h>
