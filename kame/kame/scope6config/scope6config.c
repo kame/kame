02f235ebef138d87bce06fdd68692b75f5c9b7f2
jinmei <jinmei>
2000-04-21 12:30:21 +0000

    include net/if_var.h for freebsd >= 3

--- kame/kame/scope6config/scope6config.c
+++ kame/kame/scope6config/scope6config.c
@@ -32,6 +32,9 @@
 #include <sys/ioctl.h>
 
 #include <net/if.h>
+#if defined(__FreeBSD__) && __FreeBSD__ >= 3
+#include <net/if_var.h>
+#endif
 
 #include <netinet/in.h>
 #include <netinet6/in6_var.h>
