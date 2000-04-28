f5feda8a563ef5ee93901db7cb27b408b20a1131
itojun <itojun>
2000-04-28 13:48:15 +0000

    AF_INET6 case is lacking in ALTQ code.

--- freebsd4/sys/net/if_loop.c
+++ freebsd4/sys/net/if_loop.c
@@ -375,6 +375,13 @@ lo_altqstart(ifp)
 			isr = NETISR_IP;
 			break;
 #endif
+#ifdef INET6
+		case AF_INET6:
+			m->m_flags |= M_LOOP;
+			ifq = &ip6intrq;
+			isr = NETISR_IPV6;
+			break;
+#endif
 #ifdef IPX
 		case AF_IPX:
 			ifq = &ipxintrq;
