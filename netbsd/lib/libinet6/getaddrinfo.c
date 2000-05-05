726d5305aef8ed8f1f01c3d0862b4959905ec787
itojun <itojun>
2000-05-05 07:40:21 +0000

    addrconfig() is used in explore_null(), so #ifdef AI_ADDRCONFIG is irrelevant

--- netbsd/lib/libinet6/getaddrinfo.c
+++ netbsd/lib/libinet6/getaddrinfo.c
@@ -1,4 +1,4 @@
-/*	$KAME: getaddrinfo.c,v 1.19 2000/05/01 02:29:29 itojun Exp $	*/
+/*	$KAME: getaddrinfo.c,v 1.20 2000/05/05 07:40:21 itojun Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -997,7 +997,6 @@ find_afd(af)
 	return NULL;
 }
 
-#ifdef AI_ADDRCONFIG
 /*
  * post-2553: AI_ADDRCONFIG check.  if we use getipnodeby* as backend, backend
  * will take care of it.
@@ -1019,7 +1018,6 @@ addrconfig(pai)
 	close(s);
 	return 1;
 }
-#endif
 
 #ifdef INET6
 /* convert a string to a scope identifier. XXX: IPv6 specific */
