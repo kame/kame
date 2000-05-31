e3975a636e59e736a8a401721bed9a21e97a72ce
jinmei <jinmei>
2000-05-31 05:03:09 +0000

    ip6_mloopback: make a deep copy only in M_EXT case

--- kame/sys/netinet6/ip6_output.c
+++ kame/sys/netinet6/ip6_output.c
@@ -1,4 +1,4 @@
-/*	$KAME: ip6_output.c,v 1.108 2000/05/30 14:57:44 itojun Exp $	*/
+/*	$KAME: ip6_output.c,v 1.109 2000/05/31 05:03:09 jinmei Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -3222,34 +3222,43 @@ ip6_mloopback(ifp, m, dst)
 {
 	struct mbuf *copym;
 	struct ip6_hdr *ip6;
-	struct ip6_exthdrs exthdrs;
 
 	copym = m_copy(m, 0, M_COPYALL);
 	if (copym == NULL)
 		return;
+
+	/*
+	 * Make sure to deep-copy IPv6 header portion in case the data
+	 * is in an mbuf cluster, so that we can safely override the IPv6
+	 * header portion later.
+	 */
+	if ((copym->m_flags & M_EXT) != 0 ||
+	    copym->m_len < sizeof(struct ip6_hdr)) {
+		copym = m_pullup(copym, sizeof(struct ip6_hdr));
+		if (copym == NULL)
+			return;
+	}
+
+#ifdef DIAGNOSTIC
+	if (copym->m_len < sizeof(*ip6)) {
+		m_freem(copym);
+		return;
+	}
+#endif
+
 #ifndef FAKE_LOOPBACK_IF
 	if ((ifp->if_flags & IFF_LOOPBACK) == 0)
 #else
 	if (1)
 #endif
 	{
-		/* make sure to deep-copy IPv6 header portion */
-		bzero(&exthdrs, sizeof(exthdrs));
-		if (ip6_splithdr(copym, &exthdrs) != 0)
-			return;
-		copym = exthdrs.ip6e_ip6;
-#ifdef DIAGNOSTIC
-		if (copym->m_len < sizeof(*ip6)) {
-			m_freem(copym);
-			return;
-		}
-#endif
 		ip6 = mtod(copym, struct ip6_hdr *);
 		if (IN6_IS_SCOPE_LINKLOCAL(&ip6->ip6_src))
 			ip6->ip6_src.s6_addr16[1] = 0;
 		if (IN6_IS_SCOPE_LINKLOCAL(&ip6->ip6_dst))
 			ip6->ip6_dst.s6_addr16[1] = 0;
 	}
+
 #if defined(__FreeBSD__) && __FreeBSD__ >= 3
 	(void)if_simloop(ifp, copym, (struct sockaddr *)dst, NULL);
 #else
