3aa6a6eb3ed3836038f32f60ccec12322baefc13
itojun <itojun>
2000-08-03 15:24:34 +0000

    suptype bitmap: s/0x000f/0x0000000f/ as it is 32bit value (just for readability)

--- kame/sys/netinet6/icmp6.c
+++ kame/sys/netinet6/icmp6.c
@@ -1,4 +1,4 @@
-/*	$KAME: icmp6.c,v 1.130 2000/08/03 14:22:10 itojun Exp $	*/
+/*	$KAME: icmp6.c,v 1.131 2000/08/03 15:24:34 itojun Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -1382,7 +1382,7 @@ ni6_input(m, off)
 		nni6->ni_code = ICMP6_NI_SUCCESS;
 		nni6->ni_flags = htons(0x0000);	/* raw bitmap */
 		/* supports NOOP, SUPTYPES, FQDN, and NODEADDR */
-		*(u_int32_t *)(nni6 + 1) = htonl(0x000f);
+		*(u_int32_t *)(nni6 + 1) = htonl(0x0000000f);
 		break;
 	case NI_QTYPE_FQDN:
 		nni6->ni_code = ICMP6_NI_SUCCESS;
