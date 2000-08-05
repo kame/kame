3e20e3bd51c3f4ac134281943b55a7648fbb5cc7
sumikawa <sumikawa>
2000-08-05 03:22:47 +0000

    Initial revision

---
+++ freebsd4/sys/alpha/include/smp.h
@@ -0,0 +1,10 @@
+/*
+ * $FreeBSD: src/sys/alpha/include/smp.h,v 1.1.2.1 2000/05/24 14:20:58 gallatin Exp $
+ */
+#ifndef _MACHINE_SMP_H_
+#define _MACHINE_SMP_H_
+
+#define get_mplock() { }
+#define rel_mplock() { }
+
+#endif
