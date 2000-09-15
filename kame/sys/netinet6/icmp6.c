f5b52c803e68905c376da813fb6b8b882b7d96d7
jinmei <jinmei>
2000-09-15 07:36:18 +0000

    in the check for the incoming redirect message, examine the gateway (from the routing table) only when the address family of the gateway is AF_INET6.

--- kame/sys/netinet6/icmp6.c
+++ kame/sys/netinet6/icmp6.c
@@ -1,4 +1,4 @@
-/*	$KAME: icmp6.c,v 1.140 2000/09/06 12:31:41 jinmei Exp $	*/
+/*	$KAME: icmp6.c,v 1.141 2000/09/15 07:36:18 jinmei Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -2272,7 +2272,7 @@ icmp6_redirect_input(m, off)
 		      , 0UL
 #endif
 		      );
-	if (rt) {
+	if (rt && rt->rt_gateway && rt->rt_gateway->sa_family == AF_INET6) {
 		gw6 = &(((struct sockaddr_in6 *)rt->rt_gateway)->sin6_addr);
 		if (bcmp(&src6, gw6, sizeof(struct in6_addr)) != 0) {
 			log(LOG_ERR,
@@ -2286,8 +2286,8 @@ icmp6_redirect_input(m, off)
 		}
 	} else {
 		log(LOG_ERR,
-			"ICMP6 redirect rejected; "
-			"no route found for redirect dst: %s\n",
+			"ICMP6 redirect rejected; no route "
+			"with inet6 gateway found for redirect dst: %s\n",
 			icmp6_redirect_diag(&src6, &reddst6, &redtgt6));
 		goto freeit;
 	}
