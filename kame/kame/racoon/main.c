321e512de60b8f13efa0a26f88c2b36e9f6e7be4
itojun <itojun>
2000-09-19 01:59:56 +0000

    add missing space.

--- kame/kame/racoon/main.c
+++ kame/kame/racoon/main.c
@@ -1,4 +1,4 @@
-/*	$KAME: main.c,v 1.12 2000/09/13 04:50:27 itojun Exp $	*/
+/*	$KAME: main.c,v 1.13 2000/09/19 01:59:56 itojun Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -28,7 +28,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
-/* YIPS @(#)$Id: main.c,v 1.12 2000/09/13 04:50:27 itojun Exp $ */
+/* YIPS @(#)$Id: main.c,v 1.13 2000/09/19 01:59:56 itojun Exp $ */
 
 #include <sys/types.h>
 #include <sys/param.h>
@@ -118,7 +118,7 @@ main(ac, av)
 		"%s\n", version);
 	plog(logp, LOCATION, NULL,
 	"@(#)"
-	"This product linked software developed by the OpenSSL Project"
+	"This product linked software developed by the OpenSSL Project "
 	"for use in the OpenSSL Toolkit. (http://www.openssl.org/)"
 	"\n");
 
