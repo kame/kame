0f2d40712a263f3296f79a44253360a8a265c599
itojun <itojun>
2000-09-19 16:35:44 +0000

    add ntohl() on debug message (spi).

--- kame/kame/racoon/proposal.c
+++ kame/kame/racoon/proposal.c
@@ -1,4 +1,4 @@
-/*	$KAME: proposal.c,v 1.18 2000/09/19 07:25:32 itojun Exp $	*/
+/*	$KAME: proposal.c,v 1.19 2000/09/19 16:35:44 itojun Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -28,7 +28,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
-/* YIPS @(#)$Id: proposal.c,v 1.18 2000/09/19 07:25:32 itojun Exp $ */
+/* YIPS @(#)$Id: proposal.c,v 1.19 2000/09/19 16:35:44 itojun Exp $ */
 
 #include <sys/param.h>
 #include <sys/types.h>
@@ -782,8 +782,8 @@ printsaproto(pr)
 		"encmode=%s reqid=%d:%d)\n",
 		s_ipsecdoi_proto(pr->proto_id),
 		pr->spisize,
-		pr->spi,
-		pr->spi_p,
+		(u_int32_t)ntohl(pr->spi),
+		(u_int32_t)ntohl(pr->spi_p),
 		s_ipsecdoi_attr_v(IPSECDOI_ATTR_ENC_MODE, pr->encmode),
 		pr->reqid_in, pr->reqid_out);
 
