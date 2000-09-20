f9b044ab853d4a439e3aa414eeb6a26b09614b00
itojun <itojun>
2000-09-20 01:03:18 +0000

    put "no authentication" to authtype attribute on ipcomp negotiation

--- kame/kame/racoon/proposal.c
+++ kame/kame/racoon/proposal.c
@@ -1,4 +1,4 @@
-/*	$KAME: proposal.c,v 1.19 2000/09/19 16:35:44 itojun Exp $	*/
+/*	$KAME: proposal.c,v 1.20 2000/09/20 01:03:18 itojun Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -28,7 +28,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
-/* YIPS @(#)$Id: proposal.c,v 1.19 2000/09/19 16:35:44 itojun Exp $ */
+/* YIPS @(#)$Id: proposal.c,v 1.20 2000/09/20 01:03:18 itojun Exp $ */
 
 #include <sys/param.h>
 #include <sys/types.h>
@@ -575,6 +575,7 @@ set_satrnsbysainfo(pr, sainfo)
 
 			newtr->trns_no = t++;
 			newtr->trns_id = a->alg;
+			newtr->authtype = IPSECDOI_ATTR_AUTH_NONE; /*no auth*/
 
 			inssatrns(pr, newtr);
 		}
