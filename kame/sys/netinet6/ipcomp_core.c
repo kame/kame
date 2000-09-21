d43d8c129f06f94cb14ec5d0ddd42456907ab2c7
itojun <itojun>
2000-09-21 06:03:44 +0000

    repair infinite loop

--- kame/sys/netinet6/ipcomp_core.c
+++ kame/sys/netinet6/ipcomp_core.c
@@ -1,4 +1,4 @@
-/*	$KAME: ipcomp_core.c,v 1.16 2000/09/20 23:12:31 itojun Exp $	*/
+/*	$KAME: ipcomp_core.c,v 1.17 2000/09/21 06:03:44 itojun Exp $	*/
 
 /*
  * Copyright (C) 1999 WIDE Project.
@@ -171,6 +171,7 @@ deflate_common(m, md, lenp, mode)
 		if (p && zs.avail_in == 0) {
 			zs.next_in = mtod(p, u_int8_t *);
 			zs.avail_in = p->m_len;
+			p = p->m_next;
 			while (p && p->m_len == 0)
 				p = p->m_next;
 			if (!p) {
