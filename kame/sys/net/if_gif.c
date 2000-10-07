b2b73cf3459c87e4befc5dce19b38194226b04b1
itojun <itojun>
2000-10-07 03:56:49 +0000

    do not require matches between gif phys addr and ioctl cmd, on SIOCGIFP*ADDR.  gifconfig(8) relied upon the behavior.

--- kame/sys/net/if_gif.c
+++ kame/sys/net/if_gif.c
@@ -1,4 +1,4 @@
-/*	$KAME: if_gif.c,v 1.32 2000/10/07 03:20:55 itojun Exp $	*/
+/*	$KAME: if_gif.c,v 1.33 2000/10/07 03:56:49 itojun Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -686,7 +686,6 @@ gif_ioctl(ifp, cmd, data)
 		case SIOCGIFPDSTADDR:
 			dst = &ifr->ifr_addr;
 			size = sizeof(ifr->ifr_addr);
-			i = AF_INET;
 			break;
 #endif /* INET */
 #ifdef INET6
@@ -694,15 +693,12 @@ gif_ioctl(ifp, cmd, data)
 			dst = (struct sockaddr *)
 				&(((struct in6_ifreq *)data)->ifr_addr);
 			size = sizeof(((struct in6_ifreq *)data)->ifr_addr);
-			i = AF_INET6;
 			break;
 #endif /* INET6 */
 		default:
 			error = EADDRNOTAVAIL;
 			goto bad;
 		}
-		if (src->sa_family != i)
-			return EADDRNOTAVAIL;
 		if (src->sa_len > size)
 			return EINVAL;
 		bcopy((caddr_t)src, (caddr_t)dst, src->sa_len);
