1631bbe670d1fc59e47b25b8b78fe1576288d7b3
itojun <itojun>
2000-10-07 06:23:06 +0000

    avoid fd_set overflow attack.  from openbsd

--- kame/kame/ping6/ping6.c
+++ kame/kame/ping6/ping6.c
@@ -1,4 +1,4 @@
-/*	$KAME: ping6.c,v 1.90 2000/09/06 14:51:09 jinmei Exp $	*/
+/*	$KAME: ping6.c,v 1.91 2000/10/07 06:23:06 itojun Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -278,7 +278,8 @@ main(argc, argv)
 	struct sockaddr_in6 from;
 	struct timeval timeout;
 	struct addrinfo hints;
-	fd_set fdset;
+	fd_set *fdmaskp;
+	int fdmasks;
 	register int cc, i;
 	int ch, fromlen, hold, packlen, preload, optval, ret_ga;
 	u_char *datap, *packet;
@@ -925,9 +926,10 @@ main(argc, argv)
 		(void)setitimer(ITIMER_REAL, &itimer, NULL);
 	}
 
-	FD_ZERO(&fdset);
-	timeout.tv_sec = 0;
-	timeout.tv_usec = 10000;
+	fdmasks = howmany(s+1, NFDBITS);
+	if ((fdmaskp = malloc(fdmasks)) == NULL)
+		err(1, "malloc");
+
 	for (;;) {
 		struct msghdr m;
 		struct cmsghdr *cm;
@@ -936,8 +938,11 @@ main(argc, argv)
 
 		if (options & F_FLOOD) {
 			pinger();
-			FD_SET(s, &fdset);
-			if (select(s + 1, &fdset, NULL, NULL, &timeout) < 1)
+			timeout.tv_sec = 0;
+			timeout.tv_usec = 10000;
+			memset(fdmaskp, 0, fdmasks);
+			FD_SET(s, fdmaskp);
+			if (select(s + 1, fdmaskp, NULL, NULL, &timeout) < 1)
 				continue;
 		}
 		fromlen = sizeof(from);
