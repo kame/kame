0edad6a790ae519d916fc8a2c8744a971700cd8e
jinmei <jinmei>
2000-11-30 04:54:13 +0000

    fixed arguments to in6_embedscope()

--- openbsd/sys/netinet6/raw_ipv6.c
+++ openbsd/sys/netinet6/raw_ipv6.c
@@ -400,7 +400,8 @@ rip6_ctlinput(cmd, sa, d)
 		sa6_src.sin6_scope_id = in6_addr2scopeid(m->m_pkthdr.rcvif,
 							 &ip6->ip6_src);
 #ifndef SCOPEDROUTING
-		if (in6_embedscope(&ip6->ip6_src, &sa6_src, NULL, NULL)) {
+		if (in6_embedscope(&sa6_src.sin6_addr, &ip6->ip6_src,
+				   NULL, NULL)) {
 			/* should be impossbile */
 			printf("rip6_ctlinput: in6_embedscope failed\n");
 			return;
