01587f47e16cfad1a8d295b286398cecb1cbb2b8
itojun <itojun>
2000-12-04 07:14:25 +0000

    validate result of strdup().

--- kame/kame/ifmcstat/ifmcstat.c
+++ kame/kame/ifmcstat/ifmcstat.c
@@ -341,6 +341,10 @@ if6_addrlist(ifap)
 		KREAD(ifap0, &ifa, struct ifaddr);
 
 		nam = strdup(ifname(ifa.ifa_ifp));
+		if (!nam) {
+			fprintf(stderr, "ifmcstat: not enough core\n");
+			exit(1);
+		}
 
 		for (mkp = in6_mk.lh_first; mkp; mkp = mk.mk_entry.le_next) {
 			KREAD(mkp, &mk, struct multi6_kludge);
