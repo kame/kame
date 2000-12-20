00ff47b23b4aa6d9158d20d10f3858b77baa71dd
itojun <itojun>
2000-12-20 04:45:41 +0000

    add T_A6 and T_DNAME.

--- openbsd/include/arpa/nameser.h
+++ openbsd/include/arpa/nameser.h
@@ -183,6 +183,8 @@
 #define T_SRV		33		/* Server selection */
 #define T_ATMA		34		/* ATM Address */
 #define T_NAPTR		35		/* Naming Authority PoinTeR */
+#define T_A6		38
+#define T_DNAME		39
 	/* non standard */
 #define T_UINFO		100		/* user (finger) information */
 #define T_UID		101		/* user ID */
