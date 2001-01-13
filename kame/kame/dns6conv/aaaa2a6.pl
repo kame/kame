319459e6761b3a57692b382f4262ec09f3807eb1
jinmei <jinmei>
2001-01-13 16:42:34 +0000

    a tiny script for AAAA to A6 migration.

---
+++ kame/kame/dns6conv/aaaa2a6.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/perl
+
+require 'getopts.pl';
+do Getopts('b:');
+
+
+if (@ARGV != 2) {
+    print "Usage: aaaa2a6.pl zonefile prefixname\n";
+    exit(1);
+}
+
+$zonefile = @ARGV[0];
+$pname = @ARGV[1];
+if ($opt_b) {
+    $beg = $opt_b;
+} else {
+    $beg = 64;
+}
+
+open(ZONE, $zonefile) || die "failed to open $zonefile\n";
+
+while (<ZONE>) {
+    $line++;
+
+    # match "^DNSname". keep it for later abbreviated records.
+    # XXX: not 100% reliable.
+    if (/^(\S+)/) {
+	$name0 = $1;
+    }
+
+    # match "^[DNSname] [TTL] IN AAAA IPv6address"
+    if (/^(\S*)\s+(\S*)\s*IN\s+AAAA\s+(\S+)/) {
+	$name = $1;
+	$ttl = $2;
+	$addr = $3;
+
+	if (! $name) {
+	    if (! $name0) {
+		print "no owner can be found for a AAAA RR (line $line)\n";
+		exit(1);
+	    }
+	    $name = $name0;
+	}
+
+	$prefix = `dns6conv -b $beg $addr`;
+	if (! $prefix) {
+	    exit(1);
+	}
+	chop($prefix);
+	print "$name\t$ttl IN A6 $beg $prefix\t$pname\n";
+    }
+}
