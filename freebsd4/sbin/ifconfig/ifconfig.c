2b9adb019f92749e305f11c11088a8f5a38601b8
jinmei <jinmei>
2001-02-01 06:13:06 +0000

    supported a new modifier "autoconf" for ipv6 addresses.

--- freebsd4/sbin/ifconfig/ifconfig.c
+++ freebsd4/sbin/ifconfig/ifconfig.c
@@ -199,6 +199,8 @@ struct	cmd {
 	{ "anycast",	IN6_IFF_ANYCAST, setip6flags },
 	{ "tentative",	IN6_IFF_TENTATIVE, setip6flags },
 	{ "-tentative",	-IN6_IFF_TENTATIVE, setip6flags },
+	{ "autoconf",	IN6_IFF_AUTOCONF, setip6flags },
+	{ "-autoconf",	-IN6_IFF_AUTOCONF, setip6flags },
 	{ "vltime",     NEXTARG,        setip6vltime },
 	{ "pltime",     NEXTARG,        setip6pltime },
 #endif
@@ -1114,16 +1116,18 @@ in6_status(s, info)
 	printf("prefixlen %d ", prefix(&sin->sin6_addr,
 		sizeof(struct in6_addr)));
 
-	if (flags6 & IN6_IFF_ANYCAST)
+	if ((flags6 & IN6_IFF_ANYCAST) != 0)
 		printf("anycast ");
-	if (flags6 & IN6_IFF_TENTATIVE)
+	if ((flags6 & IN6_IFF_TENTATIVE) != 0)
 		printf("tentative ");
-	if (flags6 & IN6_IFF_DUPLICATED)
+	if ((flags6 & IN6_IFF_DUPLICATED) != 0)
 		printf("duplicated ");
-	if (flags6 & IN6_IFF_DETACHED)
+	if ((flags6 & IN6_IFF_DETACHED) != 0)
 		printf("detached ");
-	if (flags6 & IN6_IFF_DEPRECATED)
+	if ((flags6 & IN6_IFF_DEPRECATED) != 0)
 		printf("deprecated ");
+	if ((flags6 & IN6_IFF_AUTOCONF) != 0)
+		printf("autoconf ");
 
         if (scopeid)
 		printf("scopeid 0x%x ", scopeid);
--- openbsd/sbin/ifconfig/ifconfig.c
+++ openbsd/sbin/ifconfig/ifconfig.c
@@ -254,6 +254,8 @@ const struct	cmd {
 	{ "-anycast",	-IN6_IFF_ANYCAST,	0,	setia6flags },
 	{ "tentative",	IN6_IFF_TENTATIVE,	0,	setia6flags },
 	{ "-tentative",	-IN6_IFF_TENTATIVE,	0,	setia6flags },
+	{ "autoconf",	IN6_IFF_AUTOCONF,	0,	setia6flags },
+	{ "-autoconf",	-IN6_IFF_AUTOCONF,	0,	setia6flags },
 	{ "pltime",	NEXTARG,	0,		setia6pltime },
 	{ "vltime",	NEXTARG,	0,		setia6vltime },
 #endif /*INET6*/
@@ -1732,16 +1734,18 @@ in6_alias(creq)
 		if (errno != EADDRNOTAVAIL)
 			warn("SIOCGIFAFLAG_IN6");
 	} else {
-		if (ifr6.ifr_ifru.ifru_flags6 & IN6_IFF_ANYCAST)
+		if ((ifr6.ifr_ifru.ifru_flags6 & IN6_IFF_ANYCAST) != 0)
 			printf(" anycast");
-		if (ifr6.ifr_ifru.ifru_flags6 & IN6_IFF_TENTATIVE)
+		if ((ifr6.ifr_ifru.ifru_flags6 & IN6_IFF_TENTATIVE) != 0)
 			printf(" tentative");
-		if (ifr6.ifr_ifru.ifru_flags6 & IN6_IFF_DUPLICATED)
+		if ((ifr6.ifr_ifru.ifru_flags6 & IN6_IFF_DUPLICATED) != 0)
 			printf(" duplicated");
-		if (ifr6.ifr_ifru.ifru_flags6 & IN6_IFF_DETACHED)
+		if ((ifr6.ifr_ifru.ifru_flags6 & IN6_IFF_DETACHED) != 0)
 			printf(" detached");
-		if (ifr6.ifr_ifru.ifru_flags6 & IN6_IFF_DEPRECATED)
+		if ((ifr6.ifr_ifru.ifru_flags6 & IN6_IFF_DEPRECATED) != 0)
 			printf(" deprecated");
+		if ((ifr6.ifr_ifru.ifru_flags6 & IN6_IFF_AUTOCONF) != 0)
+			printf(" autoconf");
 	}
 
 	if (scopeid)
