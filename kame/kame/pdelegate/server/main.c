03275bf65f42b88db20972ff178d98ebef0e8359
itojun <itojun>
2001-03-04 23:23:47 +0000

    respond to initial request (skeleton)

--- kame/kame/pdelegate/server/main.c
+++ kame/kame/pdelegate/server/main.c
@@ -1,4 +1,4 @@
-/*	$KAME: main.c,v 1.1 2001/03/04 13:31:56 itojun Exp $	*/
+/*	$KAME: main.c,v 1.2 2001/03/04 23:23:47 itojun Exp $	*/
 
 /*
  * Copyright (C) 2001 WIDE Project.
@@ -204,6 +204,9 @@ mainloop()
 			    fromlen);
 			break;
 		case ICMP6_PR_INITIAL_REQUEST:
+			receive_ireq(s, buf, l, (struct sockaddr *)&from,
+			    fromlen);
+			break;
 		case ICMP6_PR_RENEWAL_REQUEST:
 		case ICMP6_PR_PREFIX_RETURN:
 			warnx("code not supported yet");
@@ -249,8 +252,65 @@ receive_dquery(s, rbuf, l, from, fromlen)
 	memset(&reply, 0, sizeof(reply));
 	reply.icmp6_pd_hdr.icmp6_type = ICMP6_PREFIX_DELEGATION;
 	reply.icmp6_pd_hdr.icmp6_code = ICMP6_PD_PREFIX_DELEGATOR;
-	reply.icmp6_pd_hdr.icmp6_pd_flaglen = 0;	/* S bit = 0, global */
-	reply.icmp6_pd_rtlen = 0;	/* S bit = 0, global */
+	reply.icmp6_pd_hdr.icmp6_pd_flaglen = 0;
+	/* S bit = 0, global */
+	reply.icmp6_pd_hdr.icmp6_pd_flaglen &= ~ICMP6_PD_FLAGS_SCOPE;
+	reply.icmp6_pd_rtlen = 0;
+	if (sethops(s, 1) < 0) {
+		errx(1, "sethops");
+		/*NOTREACHED*/
+	}
+
+	if (sendto(s, &reply, sizeof(reply), 0, from, fromlen) < 0)
+		return -1;
+
+	return 0;
+}
+
+int
+receive_ireq(s, rbuf, l, from, fromlen)
+	int s;
+	const char *rbuf;
+	ssize_t l;
+	struct sockaddr *from;
+	int fromlen;
+{
+	const struct icmp6_prefix_request *p;
+	char hbuf[NI_MAXHOST];
+	struct icmp6_prefix_delegation reply;
+	char sbuf[4096];
+	int prefixlen = 64;
+	char *prefix = "3ffe:501:ffff:ffff::";	/*test*/
+
+	if (l < sizeof(*p)) {
+		warnx("too short message");
+		return -1;
+	}
+	p = (const struct icmp6_prefix_request *)rbuf;
+
+	if ((p->icmp6_pr_hdr.icmp6_pr_flaglen & ICMP6_PR_FLAGS_SCOPE) != 0) {
+		warnx("we do not serve site-local prefixes");
+		return -1;
+	}
+	if ((p->icmp6_pr_hdr.icmp6_pr_flaglen & ICMP6_PR_LEN_MASK) != 0 ||
+	    !IN6_IS_ADDR_UNSPECIFIED(&p->icmp6_pr_prefix)) {
+		warnx("invalid prefix %s/%d", inet_ntop(AF_INET6,
+		    &p->icmp6_pr_prefix, hbuf, sizeof(hbuf)),
+		    p->icmp6_pr_hdr.icmp6_pr_flaglen & ICMP6_PR_LEN_MASK);
+		return -1;
+	}
+
+	memset(&reply, 0, sizeof(reply));
+	reply.icmp6_pd_hdr.icmp6_type = ICMP6_PREFIX_DELEGATION;
+	reply.icmp6_pd_hdr.icmp6_code = ICMP6_PD_PREFIX_DELEGATOR;
+	reply.icmp6_pd_hdr.icmp6_pd_flaglen = prefixlen & ICMP6_PD_LEN_MASK;
+	/* S bit = 0, global */
+	reply.icmp6_pd_hdr.icmp6_pd_flaglen &= ~ICMP6_PD_FLAGS_SCOPE;
+	if (inet_pton(AF_INET6, prefix, &reply.icmp6_pd_prefix) != 1) {
+		warnx("cannot assign prefix %s/%d", prefix, prefixlen);
+		return -1;
+	}
+	reply.icmp6_pd_rtlen = 0;
 	if (sethops(s, 1) < 0) {
 		errx(1, "sethops");
 		/*NOTREACHED*/
