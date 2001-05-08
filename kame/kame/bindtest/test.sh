fc294ae01e30826b3ab013f839509f546bb14785
jinmei <jinmei>
2001-05-08 02:16:23 +0000

    added test cases with the IPV6_V6ONLY option.

--- kame/kame/bindtest/test.sh
+++ kame/kame/bindtest/test.sh
@@ -11,15 +11,26 @@ fi
 # Since -1 or -2 options would make some TIME_WAIT sockets, tests without the
 # SO_REUSExxx socket options must be done before others.
 ($bindtest -p $port
+$bindtest -p -6 $port
 $bindtest -A -p $port
+$bindtest -A -p -6 $port
 $bindtest -P -p $port
+$bindtest -P -p -6 $port
 $bindtest -AP -p $port
+$bindtest -AP -p -6 $port
 $bindtest -t -p $port
+$bindtest -t -p -6 $port
 $bindtest -t -A -1 -p $port
+$bindtest -t -A -1 -p -6 $port
 $bindtest -t -A -2 -p $port
+$bindtest -t -A -2 -p -6 $port
 $bindtest -t -P -1 -p $port
+$bindtest -t -P -1 -p -6 $port
 $bindtest -t -P -2 -p $port
+$bindtest -t -P -2 -p -6 $port
 $bindtest -t -AP -1 -p $port
+$bindtest -t -AP -1 -p -6 $port
 $bindtest -t -AP -2 -p $port
+$bindtest -t -AP -2 -p -6 $port
 echo
 uname -a) > $platform
