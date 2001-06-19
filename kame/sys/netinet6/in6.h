a285e6e787b510de23c8fb06605781c6a6b9cb4b
itojun <itojun>
2001-06-19 09:55:59 +0000

    remove IN6_IS_ADDR_ANY macro (outside of standard, #if 0'ed for a long time)

--- kame/sys/netinet6/in6.h
+++ kame/sys/netinet6/in6.h
@@ -1,4 +1,4 @@
-/*	$KAME: in6.h,v 1.90 2001/06/07 03:20:36 itojun Exp $	*/
+/*	$KAME: in6.h,v 1.91 2001/06/19 09:55:59 itojun Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -354,13 +354,6 @@ extern const struct in6_addr in6addr_linklocal_allrouters;
 #define IN6_IS_ADDR_MC_GLOBAL(a)	\
 	(IN6_IS_ADDR_MULTICAST(a) &&	\
 	 (__IPV6_ADDR_MC_SCOPE(a) == __IPV6_ADDR_SCOPE_GLOBAL))
-#endif
-
-/*
- * Wildcard Socket
- */
-#if 0	/*pre-RFC2553*/
-#define IN6_IS_ADDR_ANY(a)	IN6_IS_ADDR_UNSPECIFIED(a)
 #endif
 
 #ifdef _KERNEL	/*nonstandard*/
