d65da6d1b3734039cfb0e6d51f5f4c48ecc94dc5
jinmei <jinmei>
2001-07-05 06:54:59 +0000

    revert the previous change (on which I was wrong, sorry)

--- kame/sys/netinet6/ip6_input.c
+++ kame/sys/netinet6/ip6_input.c
@@ -1,4 +1,4 @@
-/*	$KAME: ip6_input.c,v 1.199 2001/07/05 06:42:17 jinmei Exp $	*/
+/*	$KAME: ip6_input.c,v 1.200 2001/07/05 06:54:59 jinmei Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -521,18 +521,20 @@ ip6_input(m)
 		struct mbuf *n;
 
 		MGETHDR(n, M_DONTWAIT, MT_HEADER);
-		if (n == NULL) {
-			m_freem(m);
-			return;	/* ENOBUFS */
-		}
-		M_COPY_PKTHDR(n, m);
-		if (m->m_pkthdr.len > MHLEN) {
+		if (n != NULL)
+			M_COPY_PKTHDR(n, m);
+		if (n != NULL && m->m_pkthdr.len > MHLEN) {
 			MCLGET(n, M_DONTWAIT);
 			if ((n->m_flags & M_EXT) == 0) {
 				m_freem(n);
 				n = NULL;
 			}
 		}
+		if (n == NULL) {
+			m_freem(m);
+			return;	/* ENOBUFS */
+		}
+
 		m_copydata(m, 0, m->m_pkthdr.len, mtod(n, caddr_t));
 		n->m_len = m->m_pkthdr.len;
 		m_freem(m);
