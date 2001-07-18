e4e21307a0b1ef6315cb45774ce6646df430423c
jinmei <jinmei>
2001-07-18 04:52:36 +0000

    comment fix (on the M_WAITOK issue)

--- kame/sys/netinet6/in6.c
+++ kame/sys/netinet6/in6.c
@@ -1,4 +1,4 @@
-/*	$KAME: in6.c,v 1.194 2001/07/15 05:19:20 jinmei Exp $	*/
+/*	$KAME: in6.c,v 1.195 2001/07/18 04:52:36 jinmei Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -978,8 +978,8 @@ in6_update_ifa(ifp, ifra, ia)
 		hostIsNew = 1;
 		/*
 		 * When in6_update_ifa() is called in a process of a received
-		 * RA, it is called under splnet().  So, we should call malloc
-		 * with M_NOWAIT.
+		 * RA, it is called under an interrupt context.  So, we should
+		 * call malloc with M_NOWAIT.
 		 */
 		ia = (struct in6_ifaddr *)
 			malloc(sizeof(*ia), M_IFADDR, M_NOWAIT);
