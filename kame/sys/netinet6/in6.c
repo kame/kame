cd955b2567198162cf6ab2f7ae1f0d4245980c75
itojun <itojun>
2001-07-23 06:09:37 +0000

    nodelocal -> interface local transition.

--- kame/sys/netinet6/in6.c
+++ kame/sys/netinet6/in6.c
@@ -1,4 +1,4 @@
-/*	$KAME: in6.c,v 1.199 2001/07/21 04:13:45 itojun Exp $	*/
+/*	$KAME: in6.c,v 1.200 2001/07/23 06:09:37 itojun Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -1207,16 +1207,13 @@ in6_update_ifa(ifp, ifra, ia)
 #endif
 
 		/*
-		 * join node-local all-nodes address, on loopback.
-		 * XXX: since "node-local" is obsoleted by interface-local,
-		 *      we have to join the group on every interface with
-		 *      some interface-boundary restriction.
-		 *
-		 * we need ff01::/32 only for loopback interface.
+		 * join interface-local all-nodes address, on loopback.
+		 * (ff01::1%ifN, and ff01::%ifN/32)
 		 */
 		mltaddr.sin6_addr = in6addr_nodelocal_allnodes;
+		mltaddr.sin6_addr.s6_addr16[1] = htons(ifp->if_index);
 		IN6_LOOKUP_MULTI(mltaddr.sin6_addr, ifp, in6m);
-		if (!in6m && (ifp->if_flags & IFF_LOOPBACK) != 0) {
+		if (!in6m) {
 			rtrequest(RTM_ADD,
 				  (struct sockaddr *)&mltaddr,
 				  (struct sockaddr *)&ia->ia_addr,
