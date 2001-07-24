dc8bef3e1cacaac9a09ed9d1a0ca5581e4acb3d6
itojun <itojun>
2001-07-24 09:20:44 +0000

    XXX on unsafe typecast

--- kame/sys/netinet6/in6.c
+++ kame/sys/netinet6/in6.c
@@ -1,4 +1,4 @@
-/*	$KAME: in6.c,v 1.211 2001/07/24 09:17:09 itojun Exp $	*/
+/*	$KAME: in6.c,v 1.212 2001/07/24 09:20:44 itojun Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -427,7 +427,7 @@ in6_control(so, cmd, data, ifp)
 {
 	struct	in6_ifreq *ifr = (struct in6_ifreq *)data;
 	struct	in6_ifaddr *ia = NULL;
-	struct	in6_aliasreq *ifra = (struct in6_aliasreq *)data;
+	struct	in6_aliasreq *ifra = NULL;
 #if !(defined(__FreeBSD__) && __FreeBSD__ >= 3)
 	time_t time_second = (time_t)time.tv_sec;
 #endif
@@ -524,6 +524,8 @@ in6_control(so, cmd, data, ifp)
 	 * on a single interface, we almost always look and check the
 	 * presense of ifra_addr, and reject invalid ones here.
 	 * It also decreases duplicated code among SIOC*_IN6 operations.
+	 *
+	 * XXX is it safe to touch in6_ifreq as ifra?
 	 */
 	if (ifra->ifra_addr.sin6_family == AF_INET6) {
 		struct sockaddr_in6 *sa6 =
