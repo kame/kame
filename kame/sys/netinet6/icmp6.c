318858adcc4701506f4632b0684332978ca995c3
itojun <itojun>
2001-07-24 01:52:07 +0000

    remove unnecessary validation of "n" in echo request

--- kame/sys/netinet6/icmp6.c
+++ kame/sys/netinet6/icmp6.c
@@ -1,4 +1,4 @@
-/*	$KAME: icmp6.c,v 1.224 2001/07/24 01:48:51 itojun Exp $	*/
+/*	$KAME: icmp6.c,v 1.225 2001/07/24 01:52:07 itojun Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -688,11 +688,9 @@ icmp6_input(mp, offp, proto)
 		}
 		nicmp6->icmp6_type = ICMP6_ECHO_REPLY;
 		nicmp6->icmp6_code = 0;
-		if (n) {
-			icmp6stat.icp6s_reflect++;
-			icmp6stat.icp6s_outhist[ICMP6_ECHO_REPLY]++;
-			icmp6_reflect(n, noff);
-		}
+		icmp6stat.icp6s_reflect++;
+		icmp6stat.icp6s_outhist[ICMP6_ECHO_REPLY]++;
+		icmp6_reflect(n, noff);
 		break;
 
 	case ICMP6_ECHO_REPLY:
