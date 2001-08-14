f4027b83dd75e99e11bfbad7f43a6c3c2a8fc7c5
sakane <sakane>
2001-08-14 15:21:05 +0000

    made a consessus of using modp values at bake off in helsinki.

--- kame/kame/racoon/oakley.h
+++ kame/kame/racoon/oakley.h
@@ -1,4 +1,4 @@
-/*	$KAME: oakley.h,v 1.24 2001/08/14 14:55:27 sakane Exp $	*/
+/*	$KAME: oakley.h,v 1.25 2001/08/14 15:21:05 sakane Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -74,10 +74,10 @@
 #define   OAKLEY_ATTR_GRP_DESC_EC2N155		3
 #define   OAKLEY_ATTR_GRP_DESC_EC2N185		4
 #define   OAKLEY_ATTR_GRP_DESC_MODP1536		5
-#define   OAKLEY_ATTR_GRP_DESC_MODP2048		65532	/* not defined yet */
-#define   OAKLEY_ATTR_GRP_DESC_MODP3072		65533	/* not defined yet */
-#define   OAKLEY_ATTR_GRP_DESC_MODP4096		65534	/* not defined yet */
-#define   OAKLEY_ATTR_GRP_DESC_MODP8192		65535	/* not defined yet */
+#define   OAKLEY_ATTR_GRP_DESC_MODP2048		43071	/* these value are */
+#define   OAKLEY_ATTR_GRP_DESC_MODP3072		43072	/* make consensus  */
+#define   OAKLEY_ATTR_GRP_DESC_MODP4096		43073	/* at the bake off */
+#define   OAKLEY_ATTR_GRP_DESC_MODP8192		43074	/* in helsinki     */
 					/*	32768 - 65535 Private Use */
 #define OAKLEY_ATTR_GRP_TYPE		5 /* B */
 #define   OAKLEY_ATTR_GRP_TYPE_MODP		1
