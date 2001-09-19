3bfbd3d1b75d71a28001e743102ee81610bee0ba
sakane <sakane>
2001-09-19 04:39:44 +0000

    fixed to crash.  there was a invalid pointer operation about argv.

--- kame/kame/rtsold/rtsold.c
+++ kame/kame/rtsold/rtsold.c
@@ -1,4 +1,4 @@
-/*	$KAME: rtsold.c,v 1.34 2001/09/19 04:34:35 sakane Exp $	*/
+/*	$KAME: rtsold.c,v 1.35 2001/09/19 04:39:44 sakane Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -303,7 +303,7 @@ main(argc, argv)
 				argc = 0;
 				argv = autoifprobe();
 				if (argv) {
-					for (argc = 0; argv[argc]; argc++) {
+					while (argc--) {
 						if (ifconfig(*argv)) {
 							errx(1, "failed to "
 							    "initialize %s",
