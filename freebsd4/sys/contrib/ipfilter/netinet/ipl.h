a2155b797ae4c55bf3c2fa5686cc8f2b61320168
keiichi <keiichi>
2001-09-25 04:57:52 +0000

    Initial revision

---
+++ freebsd4/sys/contrib/ipfilter/netinet/ipl.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 1993-2001 by Darren Reed.
+ *
+ * See the IPFILTER.LICENCE file for details on licencing.
+ *
+ * @(#)ipl.h	1.21 6/5/96
+ * $FreeBSD: src/sys/contrib/ipfilter/netinet/ipl.h,v 1.14.2.2 2001/07/28 13:05:48 darrenr Exp $
+ */
+
+#ifndef	__IPL_H__
+#define	__IPL_H__
+
+#define	IPL_VERSION	"IP Filter: v3.4.20"
+
+#endif
