949d4cac1795dbf3941067cd1ce2a39bf2f30a16
jinmei <jinmei>
2001-11-12 12:10:26 +0000

    added validation of the destination address for raw socket output.

--- freebsd4/sys/netinet6/raw_ip6.c
+++ freebsd4/sys/netinet6/raw_ip6.c
@@ -347,6 +347,10 @@ rip6_output(m, so, dstsock, control)
 	ip6 = mtod(m, struct ip6_hdr *);
 
 	/* KAME hack: embed scopeid */
+	if (ip6_use_defzone && dstsock->sin6_scope_id == 0) {
+		dstsock->sin6_scope_id =
+			scope6_addr2default(&dstsock->sin6_addr);
+	}
 	if ((error = in6_embedscope(dst, dstsock)) != 0)
 		goto bad;
 
@@ -685,11 +689,13 @@ rip6_send(struct socket *so, int flags, struct mbuf *m, struct sockaddr *nam,
 			m_freem(m);
 			return ENOTCONN;
 		}
+		if (nam->sin6_len != sizeof(*nam))
+			return(EINVAL);
+		if (nam->sin6_family != AF_INET6)
+			return(EAFNOSUPPORT);
 		tmp = *(struct sockaddr_in6 *)nam;
 		dst = &tmp;
 	}
-	if (ip6_use_defzone && dst->sin6_scope_id == 0)
-		dst->sin6_scope_id = scope6_addr2default(&dst->sin6_addr);
 	return rip6_output(m, so, dst, control);
 }
 
