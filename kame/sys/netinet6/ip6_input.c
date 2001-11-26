e9e3afc9380f0d90bdcbbd9cc29b801ec85f096b
itojun <itojun>
2001-11-26 08:34:12 +0000

    duplicate comment

--- kame/sys/netinet6/ip6_input.c
+++ kame/sys/netinet6/ip6_input.c
@@ -1,4 +1,4 @@
-/*	$KAME: ip6_input.c,v 1.231 2001/11/26 08:21:09 itojun Exp $	*/
+/*	$KAME: ip6_input.c,v 1.232 2001/11/26 08:34:12 itojun Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -952,6 +952,7 @@ ip6_input(m)
 			ia6->ia_ifa.if_ipackets++;
 			ia6->ia_ifa.if_ibytes += m->m_pkthdr.len;
 #elif defined(__bsdi__) && _BSDI_VERSION >= 199802
+			/* Count the packet in the ip address stats */
 			ia6->ia_ifa.ifa_ipackets++;
 			ia6->ia_ifa.ifa_ibytes += m->m_pkthdr.len;
 #endif
