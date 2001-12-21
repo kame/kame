a9fcbff9481b1101171da482333537412f7ab5d7
itojun <itojun>
2001-12-21 08:14:58 +0000

    microtime() is expensive

--- kame/sys/net/net_osdep.h
+++ kame/sys/net/net_osdep.h
@@ -1,4 +1,4 @@
-/*	$KAME: net_osdep.h,v 1.67 2001/12/19 13:23:57 jinmei Exp $	*/
+/*	$KAME: net_osdep.h,v 1.68 2001/12/21 08:14:58 itojun Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -144,7 +144,7 @@
  *	FreeBSD [34]
  *		time_second
  *	if you need portability, #ifdef out FreeBSD[34], or use microtime(&tv)
- *	then touch tv.tv_sec.
+ *	then touch tv.tv_sec (note: microtime is an expensive operation).
  *
  * - sysctl
  *	NetBSD, OpenBSD
