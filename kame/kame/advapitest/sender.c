1cb3f744f3bcb323fc22fba171df8f5ac9e5c30d
jinmei <jinmei>
2001-12-24 16:39:54 +0000

    - added sticky option test cases - added traffic class API test cases

--- kame/kame/advapitest/sender.c
+++ kame/kame/advapitest/sender.c
@@ -1,4 +1,4 @@
-/*	$KAME: sender.c,v 1.23 2001/12/21 04:35:25 jinmei Exp $ */
+/*	$KAME: sender.c,v 1.24 2001/12/24 16:39:54 jinmei Exp $ */
 /*
  * Copyright (C) 2000 WIDE Project.
  * All rights reserved.
@@ -51,20 +51,22 @@
 
 #define DEFPORT 9079
 
-int dsthdr1len, dsthdr2len, hbhlen;
-char *hlimp = NULL;
-int hlim;
+int dsthdr1len = -1, dsthdr2len = -1, hbhlen = -1;
+int stickydsthdr1len = -1, stickydsthdr2len = -1, stickyhbhlen = -1;
+char *hlimp, *stickyhlimp;
+char *tclassp, *stickytclassp;
+int hlim, tclass;
 
 struct msghdr msg;
-struct sockaddr *sa_next;
-int nextlen = 0;
-struct sockaddr_storage ss_next;
+struct sockaddr *sa_next, *sticky_sa_next;
+int nextlen = 0, stickynextlen = 0;
+struct sockaddr_storage ss_next, sticky_ss_next;
 struct cmsghdr *cmsgp = NULL;
 
 static int calc_opthlen __P((int));
-static void setopthdr __P((int, int));
+static void setopthdr __P((int, int, char *));
 static void usage __P((void));
-static int Pflag, mflag, fflag;
+static int Pflag, mflag;
 
 #ifndef IPV6_MINMTU
 #define IPV6_MINMTU 1280
@@ -79,43 +81,84 @@ main(argc, argv)
 	int rthlen = 0, ip6optlen = 0, hops = 0, error;
 	char *portstr = DEFAULTPORT;
 	char *finaldst;
-	char *nexthop = NULL;
+	char *nexthop = NULL, *stickynexthop = NULL;
 	struct iovec msgiov;
-	char *e, *databuf;
+	char *e, *databuf, *stickybuf;
+	int stickylen;
 	int datalen = 1, ch;
-	int minmtu = 0;
+	int minmtu = -1, stickyminmtu = -1;
+	int dontfrag = -1, stickydontfrag = -1;
 	struct addrinfo hints, *res;
 	extern int optind;
 	extern void *malloc();
-	extern char *optarg;
+	int sticky = 0;
 	int socktype = SOCK_DGRAM;
 	int proto = IPPROTO_UDP;
 
-	while ((ch = getopt(argc, argv, "D:d:fh:l:Mmn:Pp:s:")) != -1)
+#define STICKYCHECK \
+	do { \
+		sticky = 0; \
+		if (strcmp("sticky", optarg) == 0) { \
+			optarg = argv[optind++]; \
+			if (optarg == NULL || *optarg == '-') \
+				 usage(); \
+			sticky = 1; \
+		} \
+	} while (0)
+
+	while ((ch = getopt(argc, argv, "D:d:f:h:l:Mm:n:Pp:s:t:")) != -1)
 		switch(ch) {
 		case 'D':
-			dsthdr1len = atoi(optarg);
+			STICKYCHECK;
+			if (sticky)
+				stickydsthdr1len = atoi(optarg);
+			else
+				dsthdr1len = atoi(optarg);
 			break;
 		case 'd':
-			dsthdr2len = atoi(optarg);
+			STICKYCHECK;
+			if (sticky)
+				stickydsthdr2len = atoi(optarg);
+			else
+				dsthdr2len = atoi(optarg);
 			break;
 		case 'f':
-			fflag++;
+			STICKYCHECK;
+			if (sticky)
+				stickydontfrag = atoi(optarg);
+			else
+				dontfrag = atoi(optarg);
 			break;
 		case 'h':
-			hbhlen = atoi(optarg);
+			STICKYCHECK;
+			if (sticky)
+				stickyhbhlen = atoi(optarg);
+			else
+				hbhlen = atoi(optarg);
 			break;
 		case 'l':
-			hlimp = optarg;
+			STICKYCHECK;
+			if (sticky)
+				stickyhlimp = optarg;
+			else
+				hlimp = optarg;
 			break;
 		case 'M':
-			minmtu = 1;
+			STICKYCHECK;
+			if (sticky)
+				stickyminmtu = atoi(optarg);
+			else
+				minmtu = atoi(optarg);
 			break;
 		case 'm':
-			mflag++;
+ 			mflag++;
 			break;
 		case 'n':
-			nexthop = optarg;
+			STICKYCHECK;
+			if (sticky)
+				stickynexthop = optarg;
+			else
+				nexthop = optarg;
 			break;
 		case 'P':
 			Pflag++;
@@ -128,9 +171,17 @@ main(argc, argv)
 			if (datalen <= 0 || *optarg == '\0' || *e != '\0')
 				errx(1, "illegal datalen value -- %s", optarg);
 			break;
+		case 't':
+			STICKYCHECK;
+			if (sticky)
+				stickytclassp = optarg;
+			else
+				tclassp = optarg;
+			break;
 		default:
 			usage();
 		}
+#undef STICKYCHECK
 	argc -= optind;
 	argv += optind;
 
@@ -140,9 +191,9 @@ main(argc, argv)
 		errx(1, "can't allocate memory\n");
 	memset(databuf, 0, sizeof(datalen));
 
-	if (hbhlen > 0) ip6optlen += CMSG_SPACE(calc_opthlen(hbhlen));
-	if (dsthdr1len > 0) ip6optlen += CMSG_SPACE(calc_opthlen(dsthdr1len));
-	if (dsthdr2len > 0) ip6optlen += CMSG_SPACE(calc_opthlen(dsthdr2len));
+	if (hbhlen >= 0) ip6optlen += CMSG_SPACE(calc_opthlen(hbhlen));
+	if (dsthdr1len >= 0) ip6optlen += CMSG_SPACE(calc_opthlen(dsthdr1len));
+	if (dsthdr2len >= 0) ip6optlen += CMSG_SPACE(calc_opthlen(dsthdr2len));
 	if (hlimp != NULL) {
 		hlim = atoi(hlimp);
 #if 0
@@ -152,6 +203,12 @@ main(argc, argv)
 #endif
 		ip6optlen += CMSG_SPACE(sizeof(int));
 	}
+	if (dontfrag >= 0)
+		ip6optlen += CMSG_SPACE(sizeof(int));
+	if (minmtu >= 0)
+		ip6optlen += CMSG_SPACE(sizeof(int));
+	if (tclassp)
+		ip6optlen += CMSG_SPACE(sizeof(int));
 #ifdef IPV6_NEXTHOP
 	if (nexthop != NULL) {
 		memset(&hints, 0, sizeof(hints));
@@ -168,6 +225,20 @@ main(argc, argv)
 		freeaddrinfo(res);
 
 		ip6optlen += CMSG_SPACE(nextlen);
+	}
+	if (stickynexthop != NULL) {
+		memset(&hints, 0, sizeof(hints));
+		hints.ai_family = AF_INET6;
+		hints.ai_socktype = SOCK_DGRAM; /* not used */
+		hints.ai_protocol = IPPROTO_UDP; /* not used */
+		if ((error = getaddrinfo(nexthop, NULL, &hints, &res)) < 0) {
+			errx(1, "getaddrinfo for nexthop: %s",
+			     gai_strerror(error));
+		}
+		memcpy(&sticky_ss_next, res->ai_addr, res->ai_addrlen);
+		stickynextlen = res->ai_addrlen;
+		sticky_sa_next = (struct sockaddr *)&sticky_ss_next;
+		freeaddrinfo(res);
 	}
 #endif
 #ifdef IPV6_RTHDR_TYPE_0
@@ -190,35 +261,6 @@ main(argc, argv)
 	if (argc == 0)
 		usage();
 
-#ifdef IPV6_HOPLIMIT
-	if (hlimp != NULL) {
-		cmsgp->cmsg_len = CMSG_LEN(sizeof(int));
-		cmsgp->cmsg_level = IPPROTO_IPV6;
-		cmsgp->cmsg_type = IPV6_HOPLIMIT;
-
-		/* I believe there should be no alignment problem here */
-		*(int *)CMSG_DATA(cmsgp) = hlim;
-		cmsgp = CMSG_NXTHDR(&msg, cmsgp);
-	}
-#endif
-#ifdef IPV6_HOPOPTS
-	if (hbhlen > 0) setopthdr(hbhlen, IPV6_HOPOPTS);
-#endif
-#ifdef IPV6_RTHDRDSTOPTS
-	if (dsthdr1len > 0) setopthdr(dsthdr1len, IPV6_RTHDRDSTOPTS);
-#endif
-#ifdef IPV6_DSTOPTS
-	if (dsthdr2len > 0) setopthdr(dsthdr2len, IPV6_DSTOPTS);
-#endif
-#ifdef IPV6_NEXTHOP
-	if (sa_next != NULL) {
-		cmsgp->cmsg_len = CMSG_LEN(nextlen);
-		cmsgp->cmsg_level = IPPROTO_IPV6;
-		cmsgp->cmsg_type = IPV6_NEXTHOP;
-		memcpy(CMSG_DATA(cmsgp), sa_next, nextlen);
-		cmsgp = CMSG_NXTHDR(&msg, cmsgp);
-	}
-#endif
 #ifdef IPV6_RTHDR
 	if (argc > 1) {
 		struct ip6_rthdr *rthdr;
@@ -248,16 +290,6 @@ main(argc, argv)
 #endif
 	finaldst = argv[argc - 1];
 
-	if (strcmp(portstr, "echo") == 0) {
-		struct icmp6_hdr *icmp6 = (struct icmp6_hdr *)databuf;
-
-		socktype = SOCK_RAW;
-		proto = IPPROTO_ICMPV6;
-		portstr = NULL;
-
-		icmp6->icmp6_type = ICMP6_ECHO_REQUEST;
-	}
-
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = PF_INET6;
 	hints.ai_socktype = socktype;
@@ -270,6 +302,90 @@ main(argc, argv)
 	    < 0)
 		err(1, "socket");
 
+#ifdef IPV6_HOPLIMIT
+	if (hlimp != NULL) {
+		cmsgp->cmsg_len = CMSG_LEN(sizeof(int));
+		cmsgp->cmsg_level = IPPROTO_IPV6;
+		cmsgp->cmsg_type = IPV6_HOPLIMIT;
+
+		/* I believe there should be no alignment problem here */
+		*(int *)CMSG_DATA(cmsgp) = hlim;
+		cmsgp = CMSG_NXTHDR(&msg, cmsgp);
+	}
+
+	/*
+	 * rfc2292bis-03 obsoleted sticky hlim option, but we intentionally try
+	 * to set this option to see what happens.
+	 */
+	if (stickyhlimp != NULL) {
+		hlim = atoi(stickyhlimp);
+		if (setsockopt(s, IPPROTO_IPV6, IPV6_HOPLIMIT,
+			       &hlim, sizeof(hlim))) {
+			warn("setsockopt(IPV6_HOPLIMIT, %d)", hlim);
+		}
+	}
+#endif
+#ifdef IPV6_HOPOPTS
+	if (hbhlen > 0)
+		setopthdr(hbhlen, IPV6_HOPOPTS, NULL);
+	if (stickyhbhlen > 0) {
+		stickylen = calc_opthlen(stickyhbhlen);
+		if ((stickybuf = malloc(stickylen)) == NULL)
+			err(1, "malloc");
+		setopthdr(stickyhbhlen, IPV6_HOPOPTS, stickybuf);
+		if (setsockopt(s, IPPROTO_IPV6, IPV6_HOPOPTS, stickybuf,
+			       stickylen)) {
+			warn("setsockopt(IPV6_HOPOPTS)");
+		}
+		free(stickybuf);
+	}
+#endif
+#ifdef IPV6_RTHDRDSTOPTS
+	if (dsthdr1len > 0)
+		setopthdr(dsthdr1len, IPV6_RTHDRDSTOPTS, NULL);
+	if (stickyhbhlen > 0) {
+		stickylen = calc_opthlen(stickyhbhlen);
+		if ((stickybuf = malloc(stickylen)) == NULL)
+			err(1, "malloc");
+		setopthdr(stickyhbhlen, IPV6_RTHDRDSTOPTS, stickybuf);
+		if (setsockopt(s, IPPROTO_IPV6, IPV6_RTHDRDSTOPTS, stickybuf,
+			       stickylen)) {
+			warn("setsockopt(IPV6_RTHDRDSTOPTS)");
+		}
+		free(stickybuf);
+	}
+#endif
+#ifdef IPV6_DSTOPTS
+	if (dsthdr2len > 0)
+		setopthdr(dsthdr2len, IPV6_DSTOPTS, NULL);
+	if (stickyhbhlen > 0) {
+		stickylen = calc_opthlen(stickyhbhlen);
+		if ((stickybuf = malloc(stickylen)) == NULL)
+			err(1, "malloc");
+		setopthdr(stickyhbhlen, IPV6_DSTOPTS, stickybuf);
+		if (setsockopt(s, IPPROTO_IPV6, IPV6_DSTOPTS, stickybuf,
+			       stickylen)) {
+			warn("setsockopt(IPV6_DSTOPTS)");
+		}
+		free(stickybuf);
+	}
+#endif
+#ifdef IPV6_NEXTHOP
+	if (sa_next != NULL) {
+		cmsgp->cmsg_len = CMSG_LEN(nextlen);
+		cmsgp->cmsg_level = IPPROTO_IPV6;
+		cmsgp->cmsg_type = IPV6_NEXTHOP;
+		memcpy(CMSG_DATA(cmsgp), sa_next, nextlen);
+		cmsgp = CMSG_NXTHDR(&msg, cmsgp);
+	}
+	if (sticky_sa_next != NULL) {
+		if (setsockopt(s, IPPROTO_IPV6, IPV6_NEXTHOP, sticky_sa_next,
+			       stickynextlen)) {
+			warn("setsockopt(IPV6_NEXTHOP)");
+		}
+	}
+#endif
+
 #ifdef IPV6_RECVPATHMTU
 	if (mflag) {
 		int on = 1;
@@ -281,22 +397,57 @@ main(argc, argv)
 #endif
 
 #ifdef IPV6_USE_MIN_MTU
-	if (minmtu) {
-		int on = 1;
+	if (minmtu >= 0) {
+		cmsgp->cmsg_len = CMSG_LEN(sizeof(int));
+		cmsgp->cmsg_level = IPPROTO_IPV6;
+		cmsgp->cmsg_type = IPV6_USE_MIN_MTU;
 
-		if (setsockopt(s, IPPROTO_IPV6, IPV6_USE_MIN_MTU, &on,
-			       sizeof(on)) != 0)
+		/* I believe there should be no alignment problem here */
+		*(int *)CMSG_DATA(cmsgp) = minmtu;
+		cmsgp = CMSG_NXTHDR(&msg, cmsgp);
+	}
+	if (stickyminmtu >= 0) {
+		if (setsockopt(s, IPPROTO_IPV6, IPV6_USE_MIN_MTU,
+			       &stickyminmtu, sizeof(int)) != 0) {
 			err(1, "setsockopt(IPV6_USE_MIN_MTU)");
+		}
 	}
 #endif
 
 #ifdef IPV6_DONTFRAG
-	if (fflag) {
-		int on = 1;
+	if (dontfrag >= 0) {
+		cmsgp->cmsg_len = CMSG_LEN(sizeof(int));
+		cmsgp->cmsg_level = IPPROTO_IPV6;
+		cmsgp->cmsg_type = IPV6_DONTFRAG;
 
-		if (setsockopt(s, IPPROTO_IPV6, IPV6_DONTFRAG, &on,
-			       sizeof(on)) != 0)
+		/* I believe there should be no alignment problem here */
+		*(int *)CMSG_DATA(cmsgp) = dontfrag;
+		cmsgp = CMSG_NXTHDR(&msg, cmsgp);
+	}
+	if (stickydontfrag >= 0) {
+		if (setsockopt(s, IPPROTO_IPV6, IPV6_DONTFRAG, &stickydontfrag,
+			       sizeof(int)) != 0)
 			err(1, "setsockopt(IPV6_DONTFRAG)");
+	}
+#endif
+
+#ifdef IPV6_TCLASS
+	if (tclassp != NULL) {
+		tclass = atoi(tclassp);
+		cmsgp->cmsg_len = CMSG_LEN(sizeof(int));
+		cmsgp->cmsg_level = IPPROTO_IPV6;
+		cmsgp->cmsg_type = IPV6_TCLASS;
+
+		/* I believe there should be no alignment problem here */
+		*(int *)CMSG_DATA(cmsgp) = tclass;
+		cmsgp = CMSG_NXTHDR(&msg, cmsgp);
+	}
+	if (stickytclassp != NULL) {
+		tclass = atoi(stickytclassp);
+		if (setsockopt(s, IPPROTO_IPV6, IPV6_TCLASS,
+			       &tclass, sizeof(tclass))) {
+			warn("setsockopt(IPV6_TCLASS, %d)", tclass);
+		}
 	}
 #endif
 
@@ -322,6 +473,16 @@ main(argc, argv)
 	if (bind(s, (struct sockaddr *)&local, sizeof(local)) < 0)
 		err(1, "bind");
 #endif
+
+	if (strcmp(portstr, "echo") == 0) {
+		struct icmp6_hdr *icmp6 = (struct icmp6_hdr *)databuf;
+
+		socktype = SOCK_RAW;
+		proto = IPPROTO_ICMPV6;
+		portstr = NULL;
+
+		icmp6->icmp6_type = ICMP6_ECHO_REQUEST;
+	}
 
 	if (!isconnected) {
 		msg.msg_name = (void *)res->ai_addr;
@@ -368,6 +529,9 @@ calc_opthlen(optlen)
 {
 	int opthlen;
 
+	if (optlen == 0)
+		return(0);
+
 	if ((opthlen = inet6_opt_init(NULL, 0)) == -1)
 		errx(1, "inet6_opt_init(NULL) failed");
 	if ((opthlen = inet6_opt_append(NULL, 0, opthlen,
@@ -382,18 +546,25 @@ calc_opthlen(optlen)
 }
 
 static void
-setopthdr(optlen, hdrtype)
+setopthdr(optlen, hdrtype, hdrbuf)
 	int optlen, hdrtype;
+	char *hdrbuf;		/* sticky option if non NULL */
 {
 	int i, opthlen = 0, curlen;
-	char *hdrbuf, *optbuf;
+	int sticky = (hdrbuf != NULL);
+	char *optbuf;
 	void *optp = NULL;
 
 	opthlen = calc_opthlen(optlen);	/* XXX: duplicated calculation */
-	cmsgp->cmsg_len = CMSG_LEN(opthlen);
-	cmsgp->cmsg_level = IPPROTO_IPV6;
-	cmsgp->cmsg_type = hdrtype;
-	hdrbuf = CMSG_DATA(cmsgp);
+	if (!sticky) {
+		cmsgp->cmsg_len = CMSG_LEN(opthlen);
+		cmsgp->cmsg_level = IPPROTO_IPV6;
+		cmsgp->cmsg_type = hdrtype;
+		hdrbuf = CMSG_DATA(cmsgp);
+	}
+
+	if (optlen == 0)
+		goto end;
 
 	if ((curlen = inet6_opt_init(hdrbuf, opthlen)) == -1)
 		errx(1, "inet6_opt_init(opth, %d)", opthlen);
@@ -411,17 +582,29 @@ setopthdr(optlen, hdrtype)
 	if (inet6_opt_finish(hdrbuf, opthlen, curlen) == -1)
 		errx(1, "inet6_opt_finish(opthlen=%d, curlen=%d)",
 		     opthlen, curlen);
-
 	free(optbuf);
 
-	cmsgp = CMSG_NXTHDR(&msg, cmsgp);
+  end:
+	if (!sticky)
+		cmsgp = CMSG_NXTHDR(&msg, cmsgp);
 }
 
 static void
 usage()
 {
-	fprintf(stderr, "usage: sender [-d optlen] [-D optlen] [-f] "
-		"[-h optlen] [-l hoplimit] [-mM] [-n nexthop] [-P] [-p port] "
-		"[-s packetsize] IPv6addrs...\n");
+	fprintf(stderr,
+		"usage: sender [-d dstoptlen] [-d sticky sticky_dstoptlen]\n"
+		"              [-D rhdstoptlen] [-D sticky sticky_rhdstoptlen]\n"
+		"              [-f [01]] [-f sticky [01]] (dontfrag flag)\n"
+		"              [-h hbhoptlen] [-h sticky sticky_hbhoptlen]\n"
+		"              [-l hoplimit] [-l sticky sticky_hoplimit]\n"
+		"              [-m] (enable recvpathmtu)\n"
+		"              [-M [01]] [-M sticky [01]] (minmtu flag)\n"
+		"              [-n nexthop] [-n sticky sticky_nexthop]\n"
+		"              [-P] (get current path MTU)\n"
+		"              [-p portnum|\'echo\']\n"
+		"              [-s packetsize]\n"
+		"              [-t tclass] [-t sticky sticky_tclass]\n"
+		"              IPv6addrs...\n");
 	exit(1);
 }
