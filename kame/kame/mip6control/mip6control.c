474dd4c3c541e0840fded5a4cf351c82feaef80a
suz <suz>
2002-01-07 23:22:00 +0000

    makes it compilable on freebsd4. (please check the validity of this patch > sugyou-san)

--- kame/kame/mip6control/mip6control.c
+++ kame/kame/mip6control/mip6control.c
@@ -1,4 +1,4 @@
-/*	$KAME: mip6control.c,v 1.11 2002/01/07 13:41:53 k-sugyou Exp $	*/
+/*	$KAME: mip6control.c,v 1.12 2002/01/07 23:22:00 suz Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -46,7 +46,9 @@
 #include <net/if_var.h>
 #endif
 #define _KERNEL 1
+#define _LKM	1
 #include <net/if_hif.h>
+#undef _LKM
 #undef _KERNEL
 #include <netinet/in.h>
 #include <arpa/inet.h>
