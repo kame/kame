2d6a0bec4b3d8aa815bdeed0e956da3d5859c3f7
itojun <itojun>
2002-05-23 05:44:12 +0000

    in m_aux_delete, no need to chase beyond victim.  Archie Cobbs

--- kame/sys/kern/uipc_mbuf2.c
+++ kame/sys/kern/uipc_mbuf2.c
@@ -1,4 +1,4 @@
-/*	$KAME: uipc_mbuf2.c,v 1.32 2002/05/23 05:40:13 jinmei Exp $	*/
+/*	$KAME: uipc_mbuf2.c,v 1.33 2002/05/23 05:44:12 itojun Exp $	*/
 /*	$NetBSD: uipc_mbuf.c,v 1.40 1999/04/01 00:23:25 thorpej Exp $	*/
 
 /*
@@ -496,6 +496,7 @@ m_aux_delete(m, victim)
 				m->m_pkthdr.aux = n->m_next;
 			n->m_next = NULL;
 			m_free(n);
+			return;
 		} else
 			prev = n;
 		n = next;
