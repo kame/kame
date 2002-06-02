dbc96c845a9e679e1e12214362265b75d2178667
itojun <itojun>
2002-06-02 15:28:43 +0000

    oops

--- kame/kame/netstat/ipsec.c
+++ kame/kame/netstat/ipsec.c
@@ -1,5 +1,5 @@
 /*	$NetBSD: inet.c,v 1.35.2.1 1999/04/29 14:57:08 perry Exp $	*/
-/*	$KAME: ipsec.c,v 1.28 2002/06/02 15:27:40 itojun Exp $	*/
+/*	$KAME: ipsec.c,v 1.29 2002/06/02 15:28:43 itojun Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, 1998, and 1999 WIDE Project.
@@ -133,7 +133,7 @@ static struct val2str ipsec_espnames[] = {
 #ifdef SADB_X_EALG_RC5CBC
 	{ SADB_X_EALG_RC5CBC, "rc5-cbc", },
 #endif
-	{ SADB_X_EALG_unsigned long long128CBC, "cast128-cbc", },
+	{ SADB_X_EALG_CAST128CBC, "cast128-cbc", },
 	{ SADB_X_EALG_BLOWFISHCBC, "blowfish-cbc", },
 #ifdef SADB_X_EALG_RIJNDAELCBC
 	{ SADB_X_EALG_RIJNDAELCBC, "rijndael-cbc", },
