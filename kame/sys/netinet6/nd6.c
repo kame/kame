150ff938c2cfa14d6d4d7528ff94930077722b24
itojun <itojun>
2002-08-19 23:18:18 +0000

    correct copyout logic (quickhack)

--- kame/sys/netinet6/nd6.c
+++ kame/sys/netinet6/nd6.c
@@ -1,4 +1,4 @@
-/*	$KAME: nd6.c,v 1.291 2002/08/19 07:23:42 itojun Exp $	*/
+/*	$KAME: nd6.c,v 1.292 2002/08/19 23:18:18 itojun Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -2440,11 +2440,12 @@ nd6_drain()
 int
 nd6_sysctl(name, oldp, oldlenp, newp, newlen)
 	int name;
-	void *oldp;
+	void *oldp;	/* syscall arg, need copyout */
 	size_t *oldlenp;
-	void *newp;
+	void *newp;	/* syscall arg, need in */
 	size_t newlen;
 {
+	void *p;
 	size_t ol, l;
 	int error;
 
@@ -2457,27 +2458,31 @@ nd6_sysctl(name, oldp, oldlenp, newp, newlen)
 		return EINVAL;
 	ol = oldlenp ? *oldlenp : 0;
 
+	if (oldp) {
+		p = malloc(*oldlenp, M_TEMP, M_WAITOK);
+		if (!p)
+			return ENOMEM;
+	} else
+		p = NULL;
 	switch (name) {
-#ifdef ICMPV6CTL_ND6_DRLIST
 	case ICMPV6CTL_ND6_DRLIST:
-		error = fill_drlist(oldp, oldlenp, ol);
-		if (!error && oldp)
-			copyout(oldp, oldp, *oldlenp);
+		error = fill_drlist(p, oldlenp, ol);
+		if (!error && p && oldp)
+			copyout(p, oldp, *oldlenp);
 		break;
-#endif
 
-#ifdef ICMPV6CTL_ND6_PRLIST
 	case ICMPV6CTL_ND6_PRLIST:
-		error = fill_prlist(oldp, oldlenp, ol);
-		if (!error && oldp)
-			copyout(oldp, oldp, *oldlenp);
+		error = fill_prlist(p, oldlenp, ol);
+		if (!error && p && oldp)
+			copyout(p, oldp, *oldlenp);
 		break;
-#endif
 
 	default:
 		error = ENOPROTOOPT;
 		break;
 	}
+	if (p)
+		free(p, M_TEMP);
 
 	return(error);
 }
