385e9f017650667180ebeb5040f44ae901b744e6
jinmei <jinmei>
2002-09-20 13:56:03 +0000

    backed out a change in nd6_timer() commited last month wrt the "ifconfig deprecated" issue, because - it does not actually solve the reported problem.  the kernel (intentionally)   only cares about the lifetimes, and the deprecated flag does not affect the   behavior like source address selection. - the change breaks the "else" part after the "if" clause (though the breakage   does not affect the actual behavior).
    We need either
    + to prohibit users from changing the deprecated flag
    + to set the preferred lifetime to 0 when the deprecated flag is manually
      reset.

--- kame/sys/netinet6/nd6.c
+++ kame/sys/netinet6/nd6.c
@@ -1,4 +1,4 @@
-/*	$KAME: nd6.c,v 1.298 2002/09/11 02:34:18 itojun Exp $	*/
+/*	$KAME: nd6.c,v 1.299 2002/09/20 13:56:03 jinmei Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -643,8 +643,7 @@ nd6_timer(ignored_arg)
 			if (regen)
 				goto addrloop; /* XXX: see below */
 		}
-		if ((ia6->ia6_flags & IN6_IFF_DEPRECATED) != 0 ||
-		    IFA6_IS_DEPRECATED(ia6)) {
+		if (IFA6_IS_DEPRECATED(ia6)) {
 			int oldflags = ia6->ia6_flags;
 
 			ia6->ia6_flags |= IN6_IFF_DEPRECATED;
