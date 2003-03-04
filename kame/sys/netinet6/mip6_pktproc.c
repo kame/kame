858bfc586db4f6244e350ba1c620b58f38d57396
t-momose <t-momose>
2003-03-04 00:59:16 +0000

    A rule that calculation Kbm with only home keygen token on deregistration is also necessary for Binding Ack.

--- kame/sys/netinet6/mip6_pktproc.c
+++ kame/sys/netinet6/mip6_pktproc.c
@@ -1,4 +1,4 @@
-/*	$KAME: mip6_pktproc.c,v 1.110 2003/02/24 11:24:12 k-sugyou Exp $	*/
+/*	$KAME: mip6_pktproc.c,v 1.111 2003/03/04 00:59:16 t-momose Exp $	*/
 
 /*
  * Copyright (C) 2002 WIDE Project.  All rights reserved.
@@ -1739,10 +1739,10 @@ mip6_ip6ma_create(pktopt_mobility, src, dst, dstcoa, status, seqno, lifetime, re
 		refresh_size = 0;
 	}
 	if (mopt && 
-	    (mopt->valid_options & (MOPT_NONCE_IDX | MOPT_AUTHDATA)) &&
+	    (mopt->valid_options & (MOPT_NONCE_IDX | MOPT_AUTHDATA) == (MOPT_NONCE_IDX | MOPT_AUTHDATA)) &&
 	    mip6_calculate_kbm_from_index(dst, dstcoa, 
 		mopt->mopt_ho_nonce_idx, mopt->mopt_co_nonce_idx, 
-		0, key_bm) == 0) {
+		!IS_REQUEST_TO_CACHE(lifetime, dst, dstcoa), key_bm) == 0) {
 		need_auth = 1;
 		/* Binding Auth Option no longer require any alignment. 
 		   (6.2.7) */
