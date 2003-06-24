8a47e661703f73b3011573704a39ecc2cd899771
itojun <itojun>
2003-06-24 08:30:38 +0000

    recover code to require exact rt_key match for rtm_change/lock.

--- netbsd/sys/net/rtsock.c
+++ netbsd/sys/net/rtsock.c
@@ -331,6 +331,22 @@ route_output(m, va_alist)
 		}
 #endif
 		rt->rt_refcnt++;
+		if (rtm->rtm_type != RTM_GET) {/* XXX: too grotty */
+			struct radix_node *rn;
+			extern struct radix_node_head *mask_rnhead;
+
+			if (Bcmp(dst, rt_key(rt), dst->sa_len) != 0)
+				senderr(ESRCH);
+			if (netmask && (rn = rn_search(netmask,
+					    mask_rnhead->rnh_treetop)))
+				netmask = (struct sockaddr *)rn->rn_key;
+			for (rn = rt->rt_nodes; rn; rn = rn->rn_dupedkey)
+				if (netmask == (struct sockaddr *)rn->rn_mask)
+					break;
+			if (rn == 0)
+				senderr(ETOOMANYREFS);
+			rt = (struct rtentry *)rn;
+		}
 
 		switch(rtm->rtm_type) {
 
