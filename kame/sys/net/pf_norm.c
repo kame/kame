c9e96bba60aab2709e7a97f9cced28779985a108
itojun <itojun>
2003-07-12 09:40:10 +0000

    avoid compiler warning (u_int16_t never goes over MAXPACKET).  sync w/ openbsd

--- kame/sys/net/pf_norm.c
+++ kame/sys/net/pf_norm.c
@@ -991,12 +991,12 @@ pf_normalize_ip(struct mbuf **m0, int dir, struct ifnet *ifp, u_short *reason)
 		goto bad;
 	}
 
-	max = fragoff + ip_len;
 	/* Respect maximum length */
-	if (max > IP_MAXPACKET) {
-		DPFPRINTF(("max packet %d\n", max));
+	if (fragoff + ip_len > IP_MAXPACKET) {
+		DPFPRINTF(("max packet %d\n", fragoff + ip_len));
 		goto bad;
 	}
+	max = fragoff + ip_len; 
 
 	if ((r->rule_flag & (PFRULE_FRAGCROP|PFRULE_FRAGDROP)) == 0) {
 		/* Fully buffer all of the fragments */
