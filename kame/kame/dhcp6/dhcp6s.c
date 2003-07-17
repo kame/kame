d18b2ca770d26a4e5b5f917fa302bad1d014338c
suz <suz>
2003-07-17 07:34:35 +0000

    fixed typo

--- kame/kame/dhcp6/dhcp6s.c
+++ kame/kame/dhcp6/dhcp6s.c
@@ -1,4 +1,4 @@
-/*	$KAME: dhcp6s.c,v 1.102 2003/07/14 09:28:06 jinmei Exp $	*/
+/*	$KAME: dhcp6s.c,v 1.103 2003/07/17 07:34:35 suz Exp $	*/
 /*
  * Copyright (C) 1998 and 1999 WIDE Project.
  * All rights reserved.
@@ -574,7 +574,7 @@ server6_recv(s)
 	}
 
 	/*
-	 * A server nemver receives a relay reply message.  Since a relay
+	 * A server never receives a relay reply message.  Since a relay
 	 * replay messages will annoy option parser below, we explicitly
 	 * reject them here.
 	 */
