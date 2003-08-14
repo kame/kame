6237a654b2f3e9dd4ee0f5d6b0e7ddf7008964c2
t-momose <t-momose>
2003-08-14 06:33:07 +0000

    Removed unneccesary macro definitions. They are defined in netinet/ip6.h

--- kame/sys/netinet6/mip6.h
+++ kame/sys/netinet6/mip6.h
@@ -1,4 +1,4 @@
-/*	$KAME: mip6.h,v 1.59 2003/04/24 02:28:39 keiichi Exp $	*/
+/*	$KAME: mip6.h,v 1.60 2003/08/14 06:33:07 t-momose Exp $	*/
 
 /*
  * Copyright (C) 2001 WIDE Project.  All rights reserved.
@@ -42,20 +42,5 @@
 #define MIP6_BU_MAX_BACKOFF        7
 #define MIP6_MAX_MOB_PFX_ADV_INTERVAL	86400
 #define MIP6_MIN_MOB_PFX_ADV_INTERVAL	  600
-
-/* binding ack status code. */
-#define IP6MA_STATUS_ACCEPTED              0	/* Binding Update accepted */
-#define IP6MA_STATUS_ERRORBASE             128	/* ERROR BASE */
-#define IP6MA_STATUS_UNSPECIFIED           128	/* Reason unspecified */
-#define IP6MA_STATUS_PROHIBIT              129	/* Administratively prohibited */
-#define IP6MA_STATUS_RESOURCES             130	/* Insufficient resources */
-#define IP6MA_STATUS_NOT_SUPPORTED         131	/* Home registration not supported */
-#define IP6MA_STATUS_NOT_HOME_SUBNET       132	/* Not home subnet */
-#define IP6MA_STATUS_NOT_HOME_AGENT        133	/* Not home agent for this mobile node */
-#define IP6MA_STATUS_DAD_FAILED            134	/* Duplicate Address Detection failed */
-#define IP6MA_STATUS_SEQNO_TOO_SMALL       135	/* Sequence number out of window */
-#define IP6MA_STATUS_HOME_NONCE_EXPIRED    136	/* Expired Home Nonce Index */
-#define IP6MA_STATUS_CAREOF_NONCE_EXPIRED  137	/* Expired Care-of Nonce Index */
-#define IP6MA_STATUS_NONCE_EXPIRED         138	/* Expired Nonces */
 
 #endif /* !_MIP6_H_ */
