3c2d7a85023857b1f2cba6fafe4c9a3ff41fe343
itojun <itojun>
2003-08-28 08:37:24 +0000

    unused #define

--- kame/sys/crypto/rijndael/rijndael_local.h
+++ kame/sys/crypto/rijndael/rijndael_local.h
@@ -1,10 +1,6 @@
-/*	$KAME: rijndael_local.h,v 1.4 2003/07/15 10:47:16 itojun Exp $	*/
+/*	$KAME: rijndael_local.h,v 1.5 2003/08/28 08:37:24 itojun Exp $	*/
 
 /* the file should not be used from outside */
 typedef u_int8_t		u8;
 typedef u_int16_t		u16;	
 typedef u_int32_t		u32;
-
-#define MAXKC RIJNDAEL_MAXKC	(256/32)
-#define MAXKB RIJNDAEL_MAXKB	(256/8)
-#define MAXNR RIJNDAEL_MAXNR	14
