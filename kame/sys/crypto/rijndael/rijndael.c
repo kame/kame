a313d24b8aab914887aa825c524629d6774f33e2
itojun <itojun>
2003-08-28 08:19:51 +0000

    cleanup rijndael API.  from netbsd-current

--- kame/sys/crypto/rijndael/rijndael.c
+++ kame/sys/crypto/rijndael/rijndael.c
@@ -1,9 +1,8 @@
-/*	$NetBSD: rijndael.h,v 1.8 2003/08/27 14:23:27 itojun Exp $	*/
-/*	$KAME: rijndael.h,v 1.4 2003/08/28 08:19:52 itojun Exp $	*/
+/*	$NetBSD: rijndael.c,v 1.6 2003/08/27 14:23:27 itojun Exp $	*/
 
-/**
- * rijndael-alg-fst.h
- *
+/**             
+ * rijndael-alg-fst.c 
+ *      
  * @version 3.0 (December 2000)
  *
  * Optimised ANSI C code for the Rijndael cipher (now AES)
@@ -11,9 +10,9 @@
  * @author Vincent Rijmen <vincent.rijmen@esat.kuleuven.ac.be>
  * @author Antoon Bosselaers <antoon.bosselaers@esat.kuleuven.ac.be>
  * @author Paulo Barreto <paulo.barreto@terra.com.br>
- *
- * This code is hereby placed in the public domain.
- *
+ *              
+ * This code is hereby placed in the public domain. 
+ *      
  * THIS SOFTWARE IS PROVIDED BY THE AUTHORS ''AS IS'' AND ANY EXPRESS
  * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -27,21 +26,32 @@
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef __RIJNDAEL_H
-#define	__RIJNDAEL_H
+#include <sys/cdefs.h>
+__KERNEL_RCSID(0, "$NetBSD: rijndael.c,v 1.6 2003/08/27 14:23:27 itojun Exp $");
 
-#include <crypto/rijndael/rijndael-alg-fst.h>
-#include <crypto/rijndael/rijndael-api-fst.h>
+#include <sys/types.h>
+#include <sys/systm.h>
 
-typedef struct {
-	int	decrypt;
-	int	Nr;		/* key-length-dependent number of rounds */
-	uint32_t ek[4 * (RIJNDAEL_MAXNR + 1)];	/* encrypt key schedule */
-	uint32_t dk[4 * (RIJNDAEL_MAXNR + 1)];	/* decrypt key schedule */
-} rijndael_ctx;
+#include <crypto/rijndael/rijndael.h>
 
-void	rijndael_set_key(rijndael_ctx *, const u_char *, int);
-void	rijndael_decrypt(const rijndael_ctx *, const u_char *, u_char *);
-void	rijndael_encrypt(const rijndael_ctx *, const u_char *, u_char *);
+void
+rijndael_set_key(rijndael_ctx *ctx, const u_char *key, int bits)
+{
 
-#endif /* __RIJNDAEL_H */
+	ctx->Nr = rijndaelKeySetupEnc(ctx->ek, key, bits);
+	rijndaelKeySetupDec(ctx->dk, key, bits);
+}
+
+void
+rijndael_decrypt(const rijndael_ctx *ctx, const u_char *src, u_char *dst)
+{
+
+	rijndaelDecrypt(ctx->dk, ctx->Nr, src, dst);
+}
+
+void
+rijndael_encrypt(const rijndael_ctx *ctx, const u_char *src, u_char *dst)
+{
+
+	rijndaelEncrypt(ctx->ek, ctx->Nr, src, dst);
+}
