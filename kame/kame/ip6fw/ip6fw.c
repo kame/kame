8e64dcb735db9096afda9414e60fcf4e2d4132d6
itojun <itojun>
2003-10-02 19:36:25 +0000

    correct unsafe use of reallo().

--- kame/kame/ip6fw/ip6fw.c
+++ kame/kame/ip6fw/ip6fw.c
@@ -1,4 +1,4 @@
-/*	$KAME: ip6fw.c,v 1.13 2001/06/22 05:51:16 itojun Exp $	*/
+/*	$KAME: ip6fw.c,v 1.14 2003/10/02 19:36:25 itojun Exp $	*/
 
 /*
  * Copyright (C) 1998, 1999, 2000 and 2001 WIDE Project.
@@ -47,7 +47,7 @@
  *
  * NEW command line interface for IP firewall facility
  *
- * $KAME: ip6fw.c,v 1.13 2001/06/22 05:51:16 itojun Exp $
+ * $KAME: ip6fw.c,v 1.14 2003/10/02 19:36:25 itojun Exp $
  *
  */
 
@@ -445,7 +445,7 @@ list(ac, av)
 	int	ac;
 	char 	**av;
 {
-	struct ip6_fw *r, *rules;
+	struct ip6_fw *r, *rules, *n;
 	int l,i;
 	unsigned long rulenum;
 	int nalloc, bytes, maxbytes;
@@ -456,10 +456,10 @@ list(ac, av)
 	bytes = nalloc;
 	maxbytes = 65536 * sizeof *rules;
 	while (bytes >= nalloc) {
-		nalloc = nalloc * 2 + 200;
-		bytes = nalloc;
-		if ((rules = realloc(rules, bytes)) == NULL)
+		if ((n = realloc(rules, nalloc * 2 + 200)) == NULL)
 			err(EX_OSERR, "realloc");
+		bytes = nalloc = nalloc * 2 + 200;
+		rules = n;
 		i = getsockopt(s, IPPROTO_IPV6, IPV6_FW_GET, rules, &bytes);
 		if ((i < 0 && errno != EINVAL) || nalloc > maxbytes)
 			err(EX_OSERR, "getsockopt(IPV6_FW_GET)");
