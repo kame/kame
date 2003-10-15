4d99e5cbfd6e24d81b56600a5b5bc7c8b9f1630e
jinmei <jinmei>
2003-10-15 00:20:52 +0000

    defined prf_ra{} outside in6_prflags to be friendly with c++. From:	  Pavlin Radoslavov <pavlin@icir.org>

--- kame/sys/netinet6/in6_var.h
+++ kame/sys/netinet6/in6_var.h
@@ -1,4 +1,4 @@
-/*	$KAME: in6_var.h,v 1.89 2003/02/07 10:17:09 suz Exp $	*/
+/*	$KAME: in6_var.h,v 1.90 2003/10/15 00:20:52 jinmei Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -288,13 +288,15 @@ struct	in6_aliasreq {
  * prefix related flags passed between kernel(NDP related part) and
  * user land command(ifconfig) and daemon(rtadvd).
  */
+struct prf_ra {
+	u_char onlink : 1;
+	u_char autonomous : 1;
+	u_char router : 1;
+	u_char reserved : 5;
+};
+
 struct in6_prflags {
-	struct prf_ra {
-		u_char onlink : 1;
-		u_char autonomous : 1;
-		u_char router : 1;
-		u_char reserved : 5;
-	} prf_ra;
+	struct prf_ra prf_ra;
 	u_char prf_reserved1;
 	u_short prf_reserved2;
 	/* want to put this on 4byte offset */
