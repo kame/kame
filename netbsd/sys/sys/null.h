787e2a892db5ceef1e61f647c4e6237ec46578f3
itojun <itojun>
2003-10-21 07:55:51 +0000

    define NULL to ((void *)0) on the userland, so that we are clean of the (mis)use of NULL as (int)0.

--- netbsd/sys/sys/null.h
+++ netbsd/sys/sys/null.h
@@ -2,12 +2,8 @@
 
 #ifndef	NULL
 #if !defined(__GNUG__) || __GNUG__ < 2 || (__GNUG__ == 2 && __GNUC_MINOR__ < 90)
-#if defined(__AUDIT__) && !defined(__cplusplus)
-/*
- * This will cause GCC to emit a warning if you attempt to use NULL
- * with some non-pointer object, return value, etc.
- */
-#define	NULL	(void *)0
+#if !defined(_KERNEL) && !defined(__cplusplus)
+#define	NULL	((void *)0)
 #else
 #define	NULL	0
 #endif /* __AUDIT__ && !__cplusplus */
