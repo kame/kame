77c476d96192815cc3236764b8ba5af987aa202c
itojun <itojun>
2003-10-22 09:38:49 +0000

    always define NULL as (void *)0, not just 0.

--- netbsd/sys/sys/null.h
+++ netbsd/sys/sys/null.h
@@ -2,7 +2,7 @@
 
 #ifndef	NULL
 #if !defined(__GNUG__) || __GNUG__ < 2 || (__GNUG__ == 2 && __GNUC_MINOR__ < 90)
-#if !defined(_KERNEL) && !defined(__cplusplus)
+#if !defined(__cplusplus)
 #define	NULL	(void *)0
 #else
 #define	NULL	0
