5ccf00a4500a21420b9266fd507711a53457d9cf
ono <ono>
2003-11-06 08:20:08 +0000

    make it compilable on freebsd5

--- kame/sys/net/pf.c
+++ kame/sys/net/pf.c
@@ -4868,7 +4868,7 @@ pf_route(struct mbuf **m, struct pf_rule *r, int dir, struct ifnet *oifp,
 	m0->m_pkthdr.csum_flags |= sw_csum;
 #endif
 	m1 = m0;
-#ifdef __FreeBSD__
+#if defined(__FreeBSD__) && __FreeBSD_version != 501000
 	error = ip_fragment(m0, ifp, ifp->if_mtu, NULL, NULL);
 #else
 	error = ip_fragment(m0, ifp, ifp->if_mtu);
