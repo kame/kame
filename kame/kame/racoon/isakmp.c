647c846309ecdff203d59ada987a99b828c07abd
sakane <sakane>
2004-03-31 03:14:39 +0000

    reject the packet if the length is too big.

--- kame/kame/racoon/isakmp.c
+++ kame/kame/racoon/isakmp.c
@@ -1,4 +1,4 @@
-/*	$KAME: isakmp.c,v 1.180 2004/03/03 05:39:58 sakane Exp $	*/
+/*	$KAME: isakmp.c,v 1.181 2004/03/31 03:14:39 sakane Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -181,6 +181,18 @@ isakmp_handler(so_isakmp)
 		plog(LLV_ERROR, LOCATION, (struct sockaddr *)&remote,
 			"packet shorter than isakmp header size.\n");
 		/* dummy receive */
+		if ((len = recvfrom(so_isakmp, (char *)&isakmp, sizeof(isakmp),
+			    0, (struct sockaddr *)&remote, &remote_len)) < 0) {
+			plog(LLV_ERROR, LOCATION, NULL,
+				"failed to receive isakmp packet\n");
+		}
+		goto end;
+	}
+
+	/* reject it if the size is tooooo big. */
+	if (ntohl(isakmp.len) > 0xffff) {
+		plog(LLV_ERROR, LOCATION, NULL,
+			"the length of the isakmp header is too big.\n");
 		if ((len = recvfrom(so_isakmp, (char *)&isakmp, sizeof(isakmp),
 			    0, (struct sockaddr *)&remote, &remote_len)) < 0) {
 			plog(LLV_ERROR, LOCATION, NULL,
