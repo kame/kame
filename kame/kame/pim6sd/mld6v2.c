53841297a02c69814e83de8bb681b8ff40950835
suz <suz>
2004-06-01 08:53:48 +0000

    refined coding style (no feature change)

--- kame/kame/pim6sd/mld6v2.c
+++ kame/kame/pim6sd/mld6v2.c
@@ -1,5 +1,5 @@
 /*
- * $KAME: mld6v2.c,v 1.15 2003/09/02 09:48:45 suz Exp $
+ * $KAME: mld6v2.c,v 1.16 2004/06/01 08:53:48 suz Exp $
  */
 
 /*
@@ -117,10 +117,12 @@ make_mld6v2_msg(int type, int code, struct sockaddr_in6 *src,
     mifi_t vifi;
     struct uvif    *v;
     struct listaddr *g = NULL;
+    struct cmsghdr *cmsgp;
 
     memset(&dst_sa, 0, sizeof(dst_sa));
     dst_sa.sin6_family = AF_INET6;
     dst_sa.sin6_len = sizeof(dst_sa);
+    dst_sa.sin6_addr = allnodes_group.sin6_addr;
 
     if ((vifi = local_address(src)) == NO_VIF) {
         IF_DEBUG(DEBUG_MLD)
@@ -129,10 +131,7 @@ make_mld6v2_msg(int type, int code, struct sockaddr_in6 *src,
     }
     v = &uvifs[vifi];
 
-    if (group==NULL)
-	dst_sa.sin6_addr = allnodes_group.sin6_addr;
-    else 
-    {
+    if (group != NULL) {
         lstsrc = check_multicastV2_listener(v, group, &g, NULL);
         if (g == NULL) {
             IF_DEBUG(DEBUG_MLD)
@@ -141,9 +140,7 @@ make_mld6v2_msg(int type, int code, struct sockaddr_in6 *src,
             return FALSE;
         }
 
-        if (sflag == SFLAGNO) 
-        {
-
+        if (sflag == SFLAGNO) {
             lstsrc = g->sources;
             while (lstsrc) {
                 /*
@@ -174,8 +171,7 @@ make_mld6v2_msg(int type, int code, struct sockaddr_in6 *src,
                 lstsrc = lstsrc->al_next;
             }
         }
-        if (sflag == SFLAGYES) 
-        {
+        if (sflag == SFLAGYES) {
             lstsrc = g->sources;
             while (lstsrc) {
                 if (lstsrc->al_checklist != MORETHANLLQI)
@@ -210,17 +206,12 @@ make_mld6v2_msg(int type, int code, struct sockaddr_in6 *src,
 
     sndmh.msg_name = (caddr_t) & dst_sa;
 
-    /*
-     * fill the misc field 
-     */
+    /* fill the misc field */
     misc |= sflag;
     if (qrv <= 7)
 	misc |= qrv;
 
-    /*
-     * XXX : hard-coding , 28 is the minimal size of the mldv2 query header
-     */
-
+    /* XXX : hard-coding, 28 is the minimal size of the mldv2 query header */
     datalen = 28 + nbsrc * sizeof(struct in6_addr);
     mhp->mld_type = type;
     mhp->mld_code = code;
@@ -235,14 +226,11 @@ make_mld6v2_msg(int type, int code, struct sockaddr_in6 *src,
 
     sndiov[0].iov_len = datalen;
 
-    /*
-     * estimate total ancillary data length 
-     */
+    /* estimate total ancillary data length */
     ctllen = 0;
     if (ifindex != -1 || src)
 	ctllen += CMSG_SPACE(sizeof(struct in6_pktinfo));
-    if (alert)
-    {
+    if (alert) {
 #ifdef USE_RFC2292BIS
 	if ((hbhlen = inet6_opt_init(NULL, 0)) == -1)
 	    log_msg(LOG_ERR, 0, "inet6_opt_init(0) failed");
@@ -258,81 +246,72 @@ make_mld6v2_msg(int type, int code, struct sockaddr_in6 *src,
 	ctllen += hbhlen;
 #endif
     }
-    /*
-     * extend ancillary data space (if necessary) 
-     */
-    if (ctlbuflen < ctllen)
-    {
+
+    /* extend ancillary data space (if necessary) */
+    if (ctlbuflen < ctllen) {
 	if (sndcmsgbuf)
 	    free(sndcmsgbuf);
 	if ((sndcmsgbuf = malloc(ctllen)) == NULL)
-	    log_msg(LOG_ERR, 0, "make_mld6_msg: malloc failed");	/* assert */
+	    log_msg(LOG_ERR, 0, "make_mld6_msg: malloc failed");
 	ctlbuflen = ctllen;
     }
-    /*
-     * store ancillary data 
-     */
-    if ((sndmh.msg_controllen = ctllen) > 0)
-    {
-	struct cmsghdr *cmsgp;
-
-	sndmh.msg_control = sndcmsgbuf;
-	cmsgp = CMSG_FIRSTHDR(&sndmh);
-
-	if (ifindex != -1 || src)
-	{
-	    struct in6_pktinfo *pktinfo;
-
-	    cmsgp->cmsg_len = CMSG_LEN(sizeof(struct in6_pktinfo));
-	    cmsgp->cmsg_level = IPPROTO_IPV6;
-	    cmsgp->cmsg_type = IPV6_PKTINFO;
-	    pktinfo = (struct in6_pktinfo *) CMSG_DATA(cmsgp);
-	    memset((caddr_t) pktinfo, 0, sizeof(*pktinfo));
-	    if (ifindex != -1)
-		pktinfo->ipi6_ifindex = ifindex;
-	    if (src)
-		pktinfo->ipi6_addr = src->sin6_addr;
-	    cmsgp = CMSG_NXTHDR(&sndmh, cmsgp);
-	}
-	if (alert)
-	{
-#ifdef USE_RFC2292BIS
-	    int             currentlen;
-	    void           *hbhbuf, *optp = NULL;
-	    u_int16_t rtalert_code;
-
-	    rtalert_code = htons(IP6OPT_RTALERT_MLD);
-
-	    cmsgp->cmsg_len = CMSG_LEN(hbhlen);
-	    cmsgp->cmsg_level = IPPROTO_IPV6;
-	    cmsgp->cmsg_type = IPV6_HOPOPTS;
-	    hbhbuf = CMSG_DATA(cmsgp);
-
-	    if ((currentlen = inet6_opt_init(hbhbuf, hbhlen)) == -1)
-		log_msg(LOG_ERR, 0, "inet6_opt_init(len = %d) failed", hbhlen);
-	    if ((currentlen = inet6_opt_append(hbhbuf, hbhlen,
-					       currentlen,
-					       IP6OPT_ROUTER_ALERT, 2,
-					       2, &optp)) == -1)
-		log_msg(LOG_ERR, 0,
-		    "inet6_opt_append(len = %d/%d) failed", currentlen, hbhlen);
-	    (void) inet6_opt_set_val(optp, 0, &rtalert_code,
-				     sizeof(rtalert_code));
-	    if (inet6_opt_finish(hbhbuf, hbhlen, currentlen) == -1)
-		log_msg(LOG_ERR, 0, "inet6_opt_finish(buf) failed");
-#else				/* old advanced API */
-	    if (inet6_option_init((void *) cmsgp, &cmsgp, IPV6_HOPOPTS))
-		log_msg(LOG_ERR, 0,	/* assert */
-		    "make_mld6_msg: inet6_option_init failed");
-	    if (inet6_option_append(cmsgp, raopt, 4, 0))
-		log_msg(LOG_ERR, 0,	/* assert */
-		    "make_mld6_msg: inet6_option_append failed");
-#endif
-	    cmsgp = CMSG_NXTHDR(&sndmh, cmsgp);
-	}
-    }
-    else
+    /* store ancillary data */
+    sndmh.msg_controllen = ctllen;
+    if (ctllen <= 0) {
 	sndmh.msg_control = NULL;	/* clear for safety */
+	return TRUE;
+    }
+
+    sndmh.msg_control = sndcmsgbuf;
+    cmsgp = CMSG_FIRSTHDR(&sndmh);
+
+    if (ifindex != -1 || src) {
+	struct in6_pktinfo *pktinfo;
+
+	cmsgp->cmsg_len = CMSG_LEN(sizeof(struct in6_pktinfo));
+	cmsgp->cmsg_level = IPPROTO_IPV6;
+	cmsgp->cmsg_type = IPV6_PKTINFO;
+	pktinfo = (struct in6_pktinfo *) CMSG_DATA(cmsgp);
+	memset((caddr_t) pktinfo, 0, sizeof(*pktinfo));
+	if (ifindex != -1)
+		pktinfo->ipi6_ifindex = ifindex;
+	if (src)
+		pktinfo->ipi6_addr = src->sin6_addr;
+	cmsgp = CMSG_NXTHDR(&sndmh, cmsgp);
+    }
+    if (alert) {
+#ifdef USE_RFC2292BIS
+	int             currentlen;
+	void           *hbhbuf, *optp = NULL;
+	u_int16_t rtalert_code;
+
+	rtalert_code = htons(IP6OPT_RTALERT_MLD);
+
+	cmsgp->cmsg_len = CMSG_LEN(hbhlen);
+	cmsgp->cmsg_level = IPPROTO_IPV6;
+	cmsgp->cmsg_type = IPV6_HOPOPTS;
+	hbhbuf = CMSG_DATA(cmsgp);
+
+	if ((currentlen = inet6_opt_init(hbhbuf, hbhlen)) == -1)
+	    log_msg(LOG_ERR, 0, "inet6_opt_init(len = %d) failed", hbhlen);
+	if ((currentlen = inet6_opt_append(hbhbuf, hbhlen, currentlen,
+					   IP6OPT_ROUTER_ALERT, 2,
+					   2, &optp)) == -1)
+	    log_msg(LOG_ERR, 0,
+		    "inet6_opt_append(len = %d/%d) failed", currentlen, hbhlen);
+        (void) inet6_opt_set_val(optp, 0, &rtalert_code, sizeof(rtalert_code));
+	if (inet6_opt_finish(hbhbuf, hbhlen, currentlen) == -1)
+	    log_msg(LOG_ERR, 0, "inet6_opt_finish(buf) failed");
+#else	/* old advanced API */
+	if (inet6_option_init((void *) cmsgp, &cmsgp, IPV6_HOPOPTS))
+	    log_msg(LOG_ERR, 0,	/* assert */
+		"make_mld6_msg: inet6_option_init failed");
+	if (inet6_option_append(cmsgp, raopt, 4, 0))
+	    log_msg(LOG_ERR, 0,	/* assert */
+		"make_mld6_msg: inet6_option_append failed");
+#endif
+	cmsgp = CMSG_NXTHDR(&sndmh, cmsgp);
+    }
 
     return TRUE;
 }
@@ -357,8 +336,7 @@ send_mld6v2(int type, int code, struct sockaddr_in6 *src,
 	dstp->sin6_scope_id = index;
 #endif
 
-    if (sendmsg(mld6_socket, &sndmh, 0) < 0)
-    {
+    if (sendmsg(mld6_socket, &sndmh, 0) < 0) {
 	if (errno == ENETDOWN)
 	    check_vif_state();
 	else
@@ -377,115 +355,92 @@ send_mld6v2(int type, int code, struct sockaddr_in6 *src,
 
 
 /*
- * given a number,an exp. size in bits and a mantisse size in bits, return 
- * the coded number value according to the code described in draft_vida_mldv2_00.txt 
- * used to compute the Maximum Response Code (sizeexp=3 bits,sizemant=12 bits ) and the 
- * Querier Query interval Code  (sizeexp=3 bits ,sizmant=4 bits ) 
- * format  : |1|...exp...|...mant...|                   
- * if the number isn't representable there is a difference between realnbr and nbr 
- * if the number is too big return the max code value with a warning 
+ * given a number, an exp. size in bits and a mantisse size in bits, return
+ * the coded number value according to the code described in 
+ * draft-vida-mld-v2-08.txt 
+ * used to compute the Maximum Response Code (exp=3bit, mant=12bit) 
+ * and the Querier Query interval Code (exp=3bit, mant=4 bit)
+ * format  : |1|...exp...|...mant...|
+ * if the number isn't representable there is a difference between realnbr 
+ * and nbr if the number is too big return the max code value with a warning 
  */
-
 unsigned int
 codafloat(unsigned int nbr, unsigned int *realnbr, unsigned int sizeexp,
 	  unsigned int sizemant)
 {
-    unsigned int    mask = 0x1;
-    unsigned int    max = 0x0;
-    unsigned int    exp = 1;	/*exp value */
-    unsigned int    tmax;	/*max code value */
-    unsigned int    mantmask = 1;	/*mantisse mask */
-    unsigned int    onebit = 1;
-    unsigned int    mant;
-    u_int16_t       code = 1;	/* code */
-    int             i;
+	unsigned int mask = 0x1;
+	unsigned int max = 0x0;
+	unsigned int exp = 1;	/*exp value */
+	unsigned int tmax;	/*max code value */
+	unsigned int mantmask = 1;	/*mantisse mask */
+	unsigned int onebit = 1;
+	unsigned int mant;
+	u_int16_t code = 1;	/* code */
+	int i;
 
+	/* compute maximal exp value */
+	for (i = 1; i < sizeexp; i++)
+		exp = (exp << 1) | 1;
 
-    /*
-     * compute maximal exp value 
-     */
-    for (i = 1; i < sizeexp; i++)
-	exp = (exp << 1) | 1;
+	/* maximum size of this number in bits (after decoding) */
+	tmax = exp + 3 + sizemant + 1;
 
-    /*
-     * maximum size of this number in bits (after decoding)
-     */
+	/* minimum value of this number */
+	code <<= sizeexp + sizemant;
+	mask <<= tmax - 1;
 
-    tmax = exp + 3 + sizemant + 1;
+	/* maximum value of this number + a mantisse masque */
+	for (i = 0; i <= sizemant; i++)
+		max = max | mask >> i;
+	for (i = 0; i < sizemant; i++)
+		mantmask = mantmask | (onebit << i);
 
-    /*
-     * minimum value of this number 
-     */
+	/* not in coded number, so just return the given number as it is */
+	if (nbr < code) {
+		code = *realnbr = nbr;
+		return code;
+	}
 
-    code <<= sizeexp + sizemant;
+	/* overflowed, so just return the possible max value */
+	if (nbr > max) {
+		*realnbr = max;
+		return codafloat(max, realnbr, sizeexp, sizemant);
+	}
 
-    mask <<= tmax - 1;
-
-    /*
-     * maximum value of this number 
-     * + a mantisse masque 
-     */
-
-    for (i = 0; i <= sizemant; i++)
-	max = max | mask >> i;
-
-    for (i = 0; i < sizemant; i++)
-	mantmask = mantmask | (onebit << i);
-
-    if (nbr >= code)
-    {
-	if (nbr <= max)
-	{
-	    while (!(nbr & mask))
-	    {
+	/* calculate the float number */
+	while (!(nbr & mask)) {
 		mask >>= 1;
 		tmax--;
-	    }
-	    exp = tmax - (sizemant + 1);
-	    mant = nbr >> exp;
-	    exp -= 3;
-
-	    /*
-	     * build code 
-	     */
-
-	    mant &= mantmask;
-	    code |= mant;
-	    code |= exp << sizemant;
-
-	    /*
-	     * compute effective value ( draft-vida-mldv2 p.11 ) 
-	     */
-
-	    onebit <<= sizemant;
-	    *realnbr = (mant | onebit) << (exp + 3);
 	}
-	else
-	{
-	    *realnbr = max;
-	    return codafloat(max, realnbr, sizeexp, sizemant);
-	}
-    }
-    else
-	code = *realnbr = nbr;
+	exp = tmax - (sizemant + 1);
+	mant = nbr >> exp;
+	exp -= 3;
 
-    return code;
+	/* build code */
+	mant &= mantmask;
+	code |= mant;
+	code |= exp << sizemant;
+
+	/* compute effective value (draft-vida-mld-v2-08.txt p.11) */
+	onebit <<= sizemant;
+	*realnbr = (mant | onebit) << (exp + 3);
+	return code;
 }
 
 unsigned int
 decodeafloat(unsigned int nbr,unsigned int sizeexp,unsigned int sizemant)
 {
-	unsigned int onebit=1;
-	unsigned int mantmask=0;
-	unsigned int mant=0;
-	unsigned int exp=0;
+	unsigned int onebit = 1;
+	unsigned int mantmask = 0;
+	unsigned int mant = 0;
+	unsigned int exp = 0;
 	int i;
 	
     	for (i = 0; i < sizemant; i++)
 		mantmask = mantmask | (onebit << i);
-	mant=nbr&mantmask;
-	exp=(nbr&~(onebit<<(sizemant+sizeexp)))>>sizemant;
+	mant = nbr & mantmask;
+	exp = (nbr & ~(onebit << (sizemant + sizeexp))) >> sizemant;
 	onebit <<= sizemant;
-	return (mant|onebit)<<(exp+3);
+	return (mant | onebit) << (exp + 3);
 }
 #endif
