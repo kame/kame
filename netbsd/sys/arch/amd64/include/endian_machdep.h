2b47d4a8b78b1e990998eb8079508018a4be016e
itojun <itojun>
2004-12-21 07:58:46 +0000

    NetBSD 2.0.0

---
+++ netbsd/sys/arch/amd64/include/endian_machdep.h
@@ -0,0 +1,14 @@
+/*	$NetBSD: endian_machdep.h,v 1.1.4.1 2004/06/14 17:58:17 tron Exp $	*/
+
+#define _BYTE_ORDER _LITTLE_ENDIAN
+
+#ifdef __GNUC__
+
+#include <machine/byte_swap.h>
+
+#define	ntohl(x)	((uint32_t)__byte_swap_long((uint32_t)(x)))
+#define	ntohs(x)	((uint16_t)__byte_swap_word((uint16_t)(x)))
+#define	htonl(x)	((uint32_t)__byte_swap_long((uint32_t)(x)))
+#define	htons(x)	((uint16_t)__byte_swap_word((uint16_t)(x)))
+
+#endif
---
+++ netbsd/sys/arch/evbppc/include/bus.h
@@ -0,0 +1,3 @@
+/*	$NetBSD: bus.h,v 1.5 2003/07/25 11:44:20 scw Exp $	*/
+
+#include <powerpc/bus.h>
