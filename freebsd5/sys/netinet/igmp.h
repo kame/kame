28489cdc64688ee934727a317993206bd37a50a0
suz <suz>
2004-12-28 02:19:59 +0000

    updated INRIA copyright (requested by Hitoshi Asaeda)

--- freebsd5/sys/netinet/igmp.h
+++ freebsd5/sys/netinet/igmp.h
@@ -37,6 +37,9 @@
 /*
  * Copyright (c) 2002 INRIA. All rights reserved.
  *
+ * Implementation of Internet Group Management Protocol, Version 3.
+ * Developed by Hitoshi Asaeda, INRIA, February 2002.
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
@@ -45,11 +48,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by INRIA and its
- *	contributors.
- * 4. Neither the name of INRIA nor the names of its contributors may be
+ * 3. Neither the name of INRIA nor the names of its contributors may be
  *    used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
@@ -64,11 +63,6 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- */
-/*
- * Implementation of Internet Group Management Protocol, Version 3.
- *
- * Developed by Hitoshi Asaeda, INRIA, February 2002.
  */
 
 #ifndef _NETINET_IGMP_H_
--- freebsd5/sys/netinet/udp_usrreq.c
+++ freebsd5/sys/netinet/udp_usrreq.c
@@ -10,7 +10,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
@@ -33,6 +33,9 @@
 /*
  * Copyright (c) 2002 INRIA. All rights reserved.
  *
+ * Implementation of Internet Group Management Protocol, Version 3.
+ * Developed by Hitoshi Asaeda, INRIA, February 2002.
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
@@ -41,11 +44,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by INRIA and its
- *	contributors.
- * 4. Neither the name of INRIA nor the names of its contributors may be
+ * 3. Neither the name of INRIA nor the names of its contributors may be
  *    used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
@@ -60,11 +59,6 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- */
-/*
- * Implementation of Internet Group Management Protocol, Version 3.
- *
- * Developed by Hitoshi Asaeda, INRIA, February 2002.
  */
 
 #include "opt_ipsec.h"
