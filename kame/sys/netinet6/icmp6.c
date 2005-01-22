06187916f3920e233c32fa1cdc1d77fbc38e0f8d
suz <suz>
2005-01-22 19:09:21 +0000

    disabled this routine, since it stops ICMPv6 error advertisements when a complete ICMPv6 packet is not available (e.g.  Fragment Reassembly Time Exceeded error)

--- kame/sys/netinet6/icmp6.c
+++ kame/sys/netinet6/icmp6.c
@@ -1,4 +1,4 @@
-/*	$KAME: icmp6.c,v 1.397 2005/01/20 08:47:45 itojun Exp $	*/
+/*	$KAME: icmp6.c,v 1.398 2005/01/22 19:09:21 suz Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -447,13 +447,17 @@ icmp6_error(m, type, code, param)
 			goto freeit;
 		}
 		m_copydata(m, off, sizeof(icp), (caddr_t)&icp);
-		if (in6_cksum(m, IPPROTO_IPV6, off, m->m_pkthdr.len - off)) {
+#if 0
+		/* still under testing */
+		if (in6_cksum(m, IPPROTO_ICMPV6, off, m->m_pkthdr.len - off)) {
 			/*
 			 * original packet is broken
 			 */
 			icmp6stat.icp6s_canterror++;
 			goto freeit;
-		} else if (icp.icmp6_type < ICMP6_ECHO_REQUEST ||
+		} else 
+#endif
+		if (icp.icmp6_type < ICMP6_ECHO_REQUEST ||
 		    icp.icmp6_type == ND_REDIRECT) {
 			/*
 			 * ICMPv6 error
