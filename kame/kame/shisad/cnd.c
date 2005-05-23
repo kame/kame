62d746db438ad832fe7496d8ad7dcb30c07372fe
keiichi <keiichi>
2005-05-23 08:20:26 +0000

    - parse config file before parsing commandline optoins. - dump pid file after daemon();

--- kame/kame/shisad/cnd.c
+++ kame/kame/shisad/cnd.c
@@ -1,4 +1,4 @@
-/*	$KAME: cnd.c,v 1.8 2005/05/20 09:23:58 keiichi Exp $	*/
+/*	$KAME: cnd.c,v 1.9 2005/05/23 08:20:26 keiichi Exp $	*/
 
 /*
  * Copyright (C) 2004 WIDE Project.
@@ -78,9 +78,12 @@ struct command_table command_table[] = {
 
 /* Global Variables */
 int mhsock, mipsock, icmp6sock;
-int debug = 0, namelookup = 1;
 struct mip6stat mip6stat;
 int homeagent_mode = 0;
+
+/* configuration variables */
+int debug = 0, namelookup = 1;
+int command_port = CND_COMMAND_PORT;
 
 static char *pid_file = CND_PIDFILE;
 
@@ -113,6 +116,13 @@ main(argc, argv)
 	int ch = 0;
 	FILE *pidfp;
 
+	/* parse configuration file and set default values. */
+	if (parse_config(CFM_CND, CND_CONFFILE) == 0) {
+		config_get_number(CFT_DEBUG, &debug, config_result);
+		config_get_number(CFT_NAMELOOKUP, &namelookup, config_result);
+		config_get_number(CFT_COMMANDPORT, &command_port, config_result);
+	}
+
 	/* get options */
 	while ((ch = getopt(argc, argv, "dn")) != -1) {
 		switch (ch) {
@@ -133,12 +143,6 @@ main(argc, argv)
 	openlog("shisad(cnd)", 0, LOG_DAEMON);
 	syslog(LOG_INFO, "-- Start CN daemon at -- \n");
 
-	/* parse configuration file and set default values. */
-	if (parse_config(CFM_CND, CND_CONFFILE) == 0) {
-		config_get_number(CFT_DEBUG, &debug, config_result);
-		config_get_number(CFT_NAMELOOKUP, &namelookup, config_result);
-	}
-
 	/* open sockets */
 	mhsock_open();
 	mipsock_open();
@@ -149,18 +153,11 @@ main(argc, argv)
 
 	/* initialization */
 	fdlist_init();
-	if (command_init("cn> ", command_table, sizeof(command_table) / sizeof(struct command_table), 7777) < 0) {
+	if (command_init("cn> ", command_table, sizeof(command_table) / sizeof(struct command_table), command_port) < 0) {
 		fprintf(stderr, "Unable to open user interface\n");
 	}
 	cn_lists_init();
 	init_nonces();
-
-	/* dump current PID */
-	pid = getpid();
-	if ((pidfp = fopen(pid_file, "w")) != NULL) {
-		fprintf(pidfp, "%d\n", pid);
-		fclose(pidfp);
-	}
 
 	/* register signal handlers. */
 	signal(SIGTERM, terminate);
@@ -168,6 +165,13 @@ main(argc, argv)
 
 	if (debug == 0)
 		daemon(0, 0);
+
+	/* dump current PID */
+	pid = getpid();
+	if ((pidfp = fopen(pid_file, "w")) != NULL) {
+		fprintf(pidfp, "%d\n", pid);
+		fclose(pidfp);
+	}
 
 	new_fd_list(mhsock, POLLIN, mh_input_common);
         new_fd_list(mipsock, POLLIN, mipsock_input_common);
