6d41f7b1b09b8d24d033db3de87d333a33a78064
jinmei <jinmei>
2005-07-23 07:41:27 +0000

    sa6_recoverscope() for SIOCGIFPSRCADDR_IN6 and SIOCGIFPSRCADDR_IN6 (not tested)

--- kame/sys/net/if_nemo.c
+++ kame/sys/net/if_nemo.c
@@ -1,4 +1,4 @@
-/*	$KAME: if_nemo.c,v 1.7 2005/07/23 07:27:15 jinmei Exp $	*/
+/*	$KAME: if_nemo.c,v 1.8 2005/07/23 07:41:27 jinmei Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
@@ -819,6 +819,13 @@ nemo_ioctl(ifp, cmd, data)
 		if (src->sa_len > size)
 			return EINVAL;
 		bcopy((caddr_t)src, (caddr_t)dst, src->sa_len);
+#ifdef INET6
+		if (dst->sa_family == AF_INET6) {
+			error = sa6_recoverscope((struct sockaddr_in6 *)dst);
+			if (error != 0)
+				goto bad;
+		}
+#endif
 		break;
 			
 	case SIOCGIFPDSTADDR:
@@ -851,6 +858,13 @@ nemo_ioctl(ifp, cmd, data)
 		if (src->sa_len > size)
 			return EINVAL;
 		bcopy((caddr_t)src, (caddr_t)dst, src->sa_len);
+#ifdef INET6
+		if (dst->sa_family == AF_INET6) {
+			error = sa6_recoverscope((struct sockaddr_in6 *)dst);
+			if (error != 0)
+				goto bad;
+		}
+#endif
 		break;
 
 	case SIOCGLIFPHYADDR:
